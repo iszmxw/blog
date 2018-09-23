f9a21cadbc8f52228e6046cf437a1f19fbc1a67a
mail@54zm.com <mail@54zm.com>
2018-09-23 21:19:07 +0800

    '制作钩子'

---
+++ app/Http/Controllers/Tooling/ToolingController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers\Tooling;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class ToolingController extends Controller
+{
+    //
+    public function get_pull()
+    {
+        dd(222);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+//工具
+Route::get('git_pull', 'Tooling\ToolingController@get_pull');

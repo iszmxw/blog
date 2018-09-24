a6ee87435a6da2fc3bf2dc2caed0430bf895a699
mail@54zm.com <mail@54zm.com>
2018-09-24 20:36:30 +0800

    '上传后台模板'

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -8,11 +8,11 @@
 	<meta http-equiv="cache-control" content="no-cache">
 	<meta http-equiv="expires" content="0">
 	<title>登录博客</title>
-	<link href="{{asset('style/admin/logins/css/default.css')}}" rel="stylesheet" type="text/css" />
+	<link href="{{asset('style/admin/login/css/default.css')}}" rel="stylesheet" type="text/css" />
 	<!--必要样式-->
-	<link href="{{asset('style/admin/logins/css/styles.css')}}" rel="stylesheet" type="text/css" />
-	<link href="{{asset('style/admin/logins/css/demo.css')}}" rel="stylesheet" type="text/css" />
-	<link href="{{asset('style/admin/logins/css/loaders.css')}}" rel="stylesheet" type="text/css" />
+	<link href="{{asset('style/admin/login/css/styles.css')}}" rel="stylesheet" type="text/css" />
+	<link href="{{asset('style/admin/login/css/demo.css')}}" rel="stylesheet" type="text/css" />
+	<link href="{{asset('style/admin/login/css/loaders.css')}}" rel="stylesheet" type="text/css" />
 </head>
 <body>
 <div class='login'>
@@ -24,20 +24,20 @@
 		<input type="hidden" id="_token" value="{{csrf_token()}}">
 		<div class='login_fields__user'>
 			<div class='icon'>
-				<img alt="" src='{{asset('style/admin/logins/img/user_icon_copy.png')}}'>
+				<img alt="" src='{{asset('style/admin/login/img/user_icon_copy.png')}}'>
 			</div>
 			<input name="username" placeholder='用户名' maxlength="16" type='text' autocomplete="off">
 			<div class='validation'>
-				<img alt="" src='{{asset('style/admin/logins/img/tick.png')}}'>
+				<img alt="" src='{{asset('style/admin/login/img/tick.png')}}'>
 			</div>
 		</div>
 		<div class='login_fields__password'>
 			<div class='icon'>
-				<img alt="" src='{{asset('style/admin/logins/img/lock_icon_copy.png')}}'>
+				<img alt="" src='{{asset('style/admin/login/img/lock_icon_copy.png')}}'>
 			</div>
 			<input name="password" placeholder='密码' maxlength="16" type='text' autocomplete="off">
 			<div class='validation'>
-				<img alt="" src='{{asset('style/admin/logins/img/tick.png')}}'>
+				<img alt="" src='{{asset('style/admin/login/img/tick.png')}}'>
 			</div>
 		</div>
 		<div class='login_fields__submit'>
@@ -62,14 +62,14 @@
 </div>
 <div class="OverWindows"></div>
 
-<link href="{{asset('style/admin/logins/layui/css/layui.css')}}" rel="stylesheet" type="text/css" />
-<script type="text/javascript" src="{{asset('style/admin/logins/js/jquery.min.js')}}"></script>
-<script type="text/javascript" src="{{asset('style/admin/logins/js/jquery-ui.min.js')}}"></script>
-<script type="text/javascript" src='{{asset('style/admin/logins/js/stopExecutionOnTimeout.js')}}'></script>
-<script type="text/javascript" src="{{asset('style/admin/logins/layui/layui.js')}}"></script>
-<script type="text/javascript" src="{{asset('style/admin/logins/js/Particleground.js')}}"></script>
-<script type="text/javascript" src="{{asset('style/admin/logins/js/Treatment.js')}}"></script>
-<script type="text/javascript" src="{{asset('style/admin/logins/js/jquery.mockjax.js')}}"></script>
+<link href="{{asset('style/admin/login/layui/css/layui.css')}}" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="{{asset('style/admin/login/js/jquery.min.js')}}"></script>
+<script type="text/javascript" src="{{asset('style/admin/login/js/jquery-ui.min.js')}}"></script>
+<script type="text/javascript" src='{{asset('style/admin/login/js/stopExecutionOnTimeout.js')}}'></script>
+<script type="text/javascript" src="{{asset('style/admin/login/layui/layui.js')}}"></script>
+<script type="text/javascript" src="{{asset('style/admin/login/js/Particleground.js')}}"></script>
+<script type="text/javascript" src="{{asset('style/admin/login/js/Treatment.js')}}"></script>
+<script type="text/javascript" src="{{asset('style/admin/login/js/jquery.mockjax.js')}}"></script>
 <script type="text/javascript">
     var ajaxmockjax = 1;//是否启用虚拟Ajax的请求响 0 不启用  1 启用
     //默认账号密码

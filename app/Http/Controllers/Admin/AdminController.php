b2d2d540581326f079e80436589ea589ee75e0c2
mail@54zm.com <mail@54zm.com>
2018-09-24 21:34:55 +0800

    '后台首页模板制作'

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -13,7 +13,7 @@ class AdminController extends Controller
     public function index(Request $request)
     {
         $user_data = $request->get('user_data');
-        dump($user_data);
+//        dump($user_data);
         return view('admin.index',['user_data'=>$user_data]);
     }
 

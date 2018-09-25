7ead9250158100cb0d8036f6081760ec45677a3f
mail@54zm.com <mail@54zm.com>
2018-09-25 20:52:30 +0800

    '首页模板制作'

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -15,7 +15,7 @@ class AdminController extends Controller
         $user_data = $request->get('user_data');
         $data['php_version'] = phpversion();
         $data['laravel_version'] = app()->version();
-        dump($data);
+//        dump($data);
         return view('admin.index',['user_data'=>$user_data,'data'=>$data]);
     }
 

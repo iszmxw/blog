078e5f89f0a09f7e882076046cd7c6c02d876b9e
mail@54zm.com <mail@54zm.com>
2018-09-25 20:39:10 +0800

    '首页模板制作'

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -14,13 +14,110 @@
 		{{--头部--}}
 		@include('admin.public.header')
 		<div class="wrapper wrapper-content">
-			<div class="middle-box text-center animated fadeInRightBig">
-				<h3 class="font-bold">这是页面内容</h3>
-				<div class="error-desc">
-					您可以在这里创建所需的任何网格布局。和任何你想象的变化布局:)<br>
-					看看主页等网站。它使用许多不同的布局。
-					<br/><a href="{{url('/')}}" class="btn btn-primary m-t">返回主页</a>
+			<div class="col-lg-4">
+				<div class="ibox float-e-margins">
+					<div class="ibox-title">
+						<h5>最新消息</h5>
+						<div class="ibox-tools">
+							<span class="label label-warning-light pull-right">10条消息未读</span>
+						</div>
+					</div>
+					<div class="ibox-content">
+
+						<div>
+							<div class="feed-activity-list">
+
+								<div class="feed-element">
+									<a href="profile.html" class="pull-left">
+										<img alt="image" class="img-circle" src="img/profile.jpg">
+									</a>
+									<div class="media-body ">
+										<small class="pull-right">5分钟</small>
+										<strong>小王</strong> 晚上要一起去吃饭吗 <br>
+										<small class="text-muted">2017.12.12 12:00</small>
+
+									</div>
+								</div>
+
+								<div class="feed-element">
+									<a href="profile.html" class="pull-left">
+										<img alt="image" class="img-circle" src="img/a2.jpg">
+									</a>
+									<div class="media-body ">
+										<small class="pull-right">2个月</small>
+										<strong>小王</strong> 查看了 <strong>晚上睡觉有必要把路由器关掉么？</strong><br>
+										<small class="text-muted">2017.12.12 12:00</small>
+									</div>
+								</div>
+								<div class="feed-element">
+									<a href="profile.html" class="pull-left">
+										<img alt="image" class="img-circle" src="img/a3.jpg">
+									</a>
+									<div class="media-body ">
+										<small class="pull-right">2小时</small>
+										<strong>小王</strong> 查看了 <strong>晚上睡觉有必要把路由器关掉么？</strong>.<br>
+										<small class="text-muted">2017.12.12 12:00</small>
+									</div>
+								</div>
+								<div class="feed-element">
+									<a href="profile.html" class="pull-left">
+										<img alt="image" class="img-circle" src="img/a4.jpg">
+									</a>
+									<div class="media-body ">
+										<small class="pull-right text-navy">5小时</small>
+										<strong>小王</strong> 查看了 <strong>晚上睡觉有必要把路由器关掉么？</strong><br>
+										<small class="text-muted">2017.12.12 12:00</small>
+										<div class="actions">
+											<a class="btn btn-xs btn-white"><i class="fa fa-thumbs-up"></i> 赞 </a>
+											<a class="btn btn-xs btn-white"><i class="fa fa-heart"></i> 喜欢</a>
+										</div>
+									</div>
+								</div>
+								<div class="feed-element">
+									<a href="profile.html" class="pull-left">
+										<img alt="image" class="img-circle" src="img/a5.jpg">
+									</a>
+									<div class="media-body ">
+										<small class="pull-right">2小时</small>
+										<strong>小王</strong> 评论了 <strong>晚上睡觉有必要把路由器关掉么？</strong><br>
+										<small class="text-muted">2017.12.12 12:00</small>
+										<div class="well">
+											人生就好像你搭乘一辆火车，沿途有不同的景色，不同的站台，和不同的人上车，也会过了这个景点到下一个景点，到下一个站台，不同的人上下车，那些都是过客，重要的是自己的旅程。
+										</div>
+										<div class="pull-right">
+											<a class="btn btn-xs btn-white"><i class="fa fa-thumbs-up"></i> 赞 </a>
+										</div>
+									</div>
+								</div>
+								<div class="feed-element">
+									<a href="profile.html" class="pull-left">
+										<img alt="image" class="img-circle" src="img/profile.jpg">
+									</a>
+									<div class="media-body ">
+										<small class="pull-right">23h ago</small>
+										<strong>小王</strong> 评论了 <strong>晚上睡觉有必要把路由器关掉么？</strong><br>
+										<small class="text-muted">2017.12.12 12:00</small>
+									</div>
+								</div>
+								<div class="feed-element">
+									<a href="profile.html" class="pull-left">
+										<img alt="image" class="img-circle" src="img/a7.jpg">
+									</a>
+									<div class="media-body ">
+										<small class="pull-right">46h ago</small>
+										<strong>小王</strong> 评论了 <strong>晚上睡觉有必要把路由器关掉么？</strong><br>
+										<small class="text-muted">2017.12.12 12:00</small>
+									</div>
+								</div>
+							</div>
+
+							<button class="btn btn-primary btn-block m-t"><i class="fa fa-arrow-down"></i> 加载更多</button>
+
+						</div>
+
+					</div>
 				</div>
+
 			</div>
 		</div>
 		{{--底部--}}

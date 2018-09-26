0765ce3e0bf0eee2cfeaf5c783f4fd64b6b78208
mail@54zm.com <mail@54zm.com>
2018-09-26 22:34:11 +0800

    '修改編輯器'

--- resources/views/admin/article_add.blade.php
+++ resources/views/admin/article_add.blade.php
@@ -37,28 +37,11 @@
 				<div class="col-lg-12">
 					<div class="ibox float-e-margins">
 						<div class="ibox-title">
-							<h5>所有表单元素 <small>有自定义复选框和radion元素。</small></h5>
-							<div class="ibox-tools">
-								<a class="collapse-link">
-									<i class="fa fa-chevron-up"></i>
-								</a>
-								<a class="dropdown-toggle" data-toggle="dropdown" href="#">
-									<i class="fa fa-wrench"></i>
-								</a>
-								<ul class="dropdown-menu dropdown-user">
-									<li><a href="#">选项 1</a>
-									</li>
-									<li><a href="#">选项 2</a>
-									</li>
-								</ul>
-								<a class="close-link">
-									<i class="fa fa-times"></i>
-								</a>
-							</div>
+							<h5>文章基本信息 <small>开始你的写作吧。</small></h5>
 						</div>
 						<div class="ibox-content">
 							<form method="get" class="form-horizontal">
-								<div class="form-group"><label class="col-sm-2 control-label">正常</label>
+								<div class="form-group"><label class="col-sm-2 control-label">标题</label>
 
 									<div class="col-sm-10"><input type="text" class="form-control"></div>
 								</div>

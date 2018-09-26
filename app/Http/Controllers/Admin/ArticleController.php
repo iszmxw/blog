74e277d5ab81d7fb10d322cf81aa6ae3e35be2b0
mail@54zm.com <mail@54zm.com>
2018-09-26 23:40:31 +0800

    '添加文章页面'

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -13,7 +13,6 @@ class ArticleController extends Controller
     public function add(Request $request)
     {
         $user_data = $request->get('user_data');
-
         return view('admin.article_add',['user_data'=>$user_data]);
     }
 }

fa00f1ad9bbac5170fdf084bc2e24061e05b1705
mail@54zm.com <mail@54zm.com>
2018-09-27 23:39:44 +0800

    添加文章

--- public/style/admin/js/article_add.js
+++ public/style/admin/js/article_add.js
@@ -7,6 +7,106 @@ $(document).ready(function () {
     });
 });
 
+$(function () {
+    var i = -1;
+    var toastCount = 0;
+    var $toastlast;
+    var getMessage = function () {
+        var msg = '嗨，欢迎来到网豫游戏。我是演示。';
+        return msg;
+    };
+
+    $('#showsimple').click(function (){
+        // Display a success toast, with a title
+        toastr.success('没有任何选择','简单的通知!')
+    });
+    $('#showtoast').click(function () {
+        var shortCutFunction = $("#toastTypeGroup input:radio:checked").val();
+        var msg = $('#message').val();
+        var title = $('#title').val() || '';
+        var $showDuration = $('#showDuration');
+        var $hideDuration = $('#hideDuration');
+        var $timeOut = $('#timeOut');
+        var $extendedTimeOut = $('#extendedTimeOut');
+        var $showEasing = $('#showEasing');
+        var $hideEasing = $('#hideEasing');
+        var $showMethod = $('#showMethod');
+        var $hideMethod = $('#hideMethod');
+        var toastIndex = toastCount++;
+        toastr.options = {
+            closeButton: $('#closeButton').prop('checked'),
+            debug: $('#debugInfo').prop('checked'),
+            progressBar: $('#progressBar').prop('checked'),
+            preventDuplicates: $('#preventDuplicates').prop('checked'),
+            positionClass: $('#positionGroup input:radio:checked').val() || 'toast-top-right',
+            onclick: null
+        };
+        if ($('#addBehaviorOnToastClick').prop('checked')) {
+            toastr.options.onclick = function () {
+                alert('消失后，您可以执行一些自定义操作');
+            };
+        }
+        if ($showDuration.val().length) {
+            toastr.options.showDuration = $showDuration.val();
+        }
+        if ($hideDuration.val().length) {
+            toastr.options.hideDuration = $hideDuration.val();
+        }
+        if ($timeOut.val().length) {
+            toastr.options.timeOut = $timeOut.val();
+        }
+        if ($extendedTimeOut.val().length) {
+            toastr.options.extendedTimeOut = $extendedTimeOut.val();
+        }
+        if ($showEasing.val().length) {
+            toastr.options.showEasing = $showEasing.val();
+        }
+        if ($hideEasing.val().length) {
+            toastr.options.hideEasing = $hideEasing.val();
+        }
+        if ($showMethod.val().length) {
+            toastr.options.showMethod = $showMethod.val();
+        }
+        if ($hideMethod.val().length) {
+            toastr.options.hideMethod = $hideMethod.val();
+        }
+        if (!msg) {
+            msg = getMessage();
+        }
+        $("#toastrOptions").text("Command: toastr["
+            + shortCutFunction
+            + "](\""
+            + msg
+            + (title ? "\", \"" + title : '')
+            + "\")\n\ntoastr.options = "
+            + JSON.stringify(toastr.options, null, 2)
+        );
+        var $toast = toastr[shortCutFunction](msg, title); // Wire up an event handler to a button in the toast, if it exists
+        $toastlast = $toast;
+        if ($toast.find('#okBtn').length) {
+            $toast.delegate('#okBtn', 'click', function () {
+                alert('you clicked me. i was toast #' + toastIndex + '. goodbye!');
+                $toast.remove();
+            });
+        }
+        if ($toast.find('#surpriseBtn').length) {
+            $toast.delegate('#surpriseBtn', 'click', function () {
+                alert('Surprise! you clicked me. i was toast #' + toastIndex + '. You could perform an action here.');
+            });
+        }
+    });
+    function getLastToast(){
+        return $toastlast;
+    }
+    $('#clearlasttoast').click(function () {
+        toastr.clear(getLastToast());
+    });
+    $('#cleartoasts').click(function () {
+        toastr.clear();
+    });
+});
+
+
 function add_data(){
     var target = $("#post_url");
     var url = target.attr("action");
@@ -17,7 +117,7 @@ function add_data(){
         if (json.status == -1) {
             window.location.reload();
         } else if(json.status == 1) {
-            Command: toastr[success]("嗨，欢迎来到网豫游戏。我是演示。");
+            $("#toastrOptions").text('Command: toastr[success]("嗨，欢迎来到网豫游戏。我是演示。")')
             // swal({
             //     title: "提示信息",
             //     text: json.data,

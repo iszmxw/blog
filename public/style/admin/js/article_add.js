2250eb48745cb13045f1a5b731247e2ff2756609
mail@54zm.com <mail@54zm.com>
2018-09-28 00:10:44 +0800

    添加文章

--- public/style/admin/js/article_add.js
+++ public/style/admin/js/article_add.js
@@ -1,3 +1,19 @@
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "progressBar": true,
+    "preventDuplicates": false,
+    "positionClass": "toast-top-center",
+    "onclick": null,
+    "showDuration": "400",
+    "hideDuration": "1000",
+    "timeOut": "7000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};
 $(document).ready(function () {
     $('.summernote').summernote({
         height: 600,

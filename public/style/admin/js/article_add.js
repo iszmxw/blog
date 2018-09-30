9f19cd79853ab961db886900b9a2881b9ec3dbea
mail@54zm.com <mail@54zm.com>
2018-09-30 20:19:43 +0800

    添加文章

--- public/style/admin/js/article_add.js
+++ public/style/admin/js/article_add.js
@@ -13,19 +13,19 @@ function add_data(){
     var summernote = $(".summernote").summernote('code');
     data = data +'&'+'content='+summernote;
     $.post(url, data, function (json) {
-        if (json.status == -1) {
-            window.location.reload();
-        } else if(json.status == 1) {
-            toastr.success("祝贺你添加成功了！");
-            setInterval(function(){
-                window.location.reload();
-            },3000);
-            return false;
-        }else{
-            toastr.error("添加失败请稍后再试！");
-            setInterval(function(){
-                window.location.reload();
-            },3000);
-        }
+        // if (json.status == -1) {
+        //     window.location.reload();
+        // } else if(json.status == 1) {
+        //     toastr.success("祝贺你添加成功了！");
+        //     setInterval(function(){
+        //         window.location.reload();
+        //     },3000);
+        //     return false;
+        // }else{
+        //     toastr.error("添加失败请稍后再试！");
+        //     setInterval(function(){
+        //         window.location.reload();
+        //     },3000);
+        // }
     });
 }

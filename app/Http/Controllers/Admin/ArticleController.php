b47a92055c7b1563cd3819f02a47caf10ff97096
mail@54zm.com <mail@54zm.com>
2018-10-05 21:32:53 +0800

    '文章列表'

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -51,7 +51,6 @@ class ArticleController extends Controller
     {
         $user_data = $request->get('user_data');
         $list = Blog::getPaginate([],['blog.gid','blog.title','sort.sortname','blog.views','blog.date'],10);
-        dump($list,1);
         return view('admin.article_list',['user_data'=>$user_data,'list'=>$list]);
     }
 }

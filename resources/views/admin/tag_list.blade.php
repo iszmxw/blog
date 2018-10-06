7bc8587a932f4e74127e8712ed2da2c2ee6e2fb6
mail@54zm.com <mail@54zm.com>
2018-10-06 22:28:08 +0800

    '文章列表'

--- resources/views/admin/tag_list.blade.php
+++ resources/views/admin/tag_list.blade.php
@@ -14,14 +14,12 @@
 		{{--头部--}}
 		@include('admin.public.header')
 		<div class="wrapper wrapper-content">
-			<div class="middle-box text-center animated fadeInRightBig">
-				<h3 class="font-bold">这是页面内容</h3>
-				<div class="error-desc">
-					您可以在这里创建所需的任何网格布局。和任何你想象的变化布局:)<br>
-					看看主页等网站。它使用许多不同的布局。
-					<br/><a href="{{url('/')}}" class="btn btn-primary m-t">返回主页</a>
-				</div>
-			</div>
+			<ul class="tag-list" style="padding: 0">
+				<li><a href=""><i class="fa fa-tag"></i> 标签</a></li>
+				<li><a href=""><i class="fa fa-tag"></i> 标签</a></li>
+				<li><a href=""><i class="fa fa-tag"></i> 标签</a></li>
+				<li><a href=""><i class="fa fa-tag"></i> 标签</a></li>
+			</ul>
 		</div>
 		{{--底部--}}
 		@include('admin.public.footer')

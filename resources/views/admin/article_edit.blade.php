59a758bde5cb3dfd0b14dd16f97fc3ed39c24540
iszmxw <mail@54zm.com>
2018-10-06 14:03:50 +0800

    '文章列表'

--- resources/views/admin/article_edit.blade.php
+++ resources/views/admin/article_edit.blade.php
@@ -38,7 +38,7 @@
                     <div class="ibox float-e-margins">
                         <div class="ibox-title">
                             <h5>文章基本信息
-                                <small>修改文章内容。{{app_path()}}</small>
+                                <small>修改文章内容。{{route('route_name')}}</small>
                             </h5>
                         </div>
                         <div class="ibox-content">

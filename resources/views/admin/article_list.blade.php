06cf34d6547d69ffd3640412d7dcec609a47d2d9
iszmxw <mail@54zm.com>
2018-10-06 09:21:59 +0800

    '文章列表'

--- resources/views/admin/article_list.blade.php
+++ resources/views/admin/article_list.blade.php
@@ -86,8 +86,8 @@
                                         <td><span class="label label-success">{{$value['views']}}</span></td>
                                         <td>{{date('Y-m-d H:i:s',$value['date'])}}</td>
                                         <td>
-                                            <button class="btn btn-info" type="button"><i class="fa fa-paste"></i>编辑</button>
-                                            <button class="btn btn-danger" type="button"><i class="fa fa-times"></i>删除</button>
+                                            <button class="btn btn-info" type="button"><i class="fa fa-paste"></i>&nbsp;&nbsp;编辑</button>
+                                            <button class="btn btn-danger" type="button"><i class="fa fa-times"></i>&nbsp;&nbsp;删除</button>
                                         </td>
                                     </tr>
                                     @endforeach

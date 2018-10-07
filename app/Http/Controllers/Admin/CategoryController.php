105afe2a5609bef310f80508c13df98ba48f22be
iszmxw <mail@54zm.com>
2018-10-07 18:07:45 +0800

    '栏目管理'

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -12,12 +12,14 @@ class CategoryController extends Controller
     //栏目分类列表
     public function category_list(Request $request)
     {
-        return view('admin.category_list');
+        $user_data = $request->get('user_data');
+        return view('admin.category_list',['user_data'=>$user_data]);
     }
 
     //首页导航栏列表
     public function navbar_list(Request $request)
     {
-        return view('admin.navbar_list');
+        $user_data = $request->get('user_data');
+        return view('admin.navbar_list',['user_data'=>$user_data]);
     }
 }

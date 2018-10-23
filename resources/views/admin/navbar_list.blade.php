3020d48406bb464dba5e44016a81c54c68235396
mail@54zm.com <mail@54zm.com>
2018-10-23 23:12:33 +0800

    '添加导航栏'

--- resources/views/admin/navbar_list.blade.php
+++ resources/views/admin/navbar_list.blade.php
@@ -118,7 +118,7 @@
 				<input type="hidden" name="_token" value="{{csrf_token()}}">
 				<div class="modal-header">
 					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
-					<h4 class="modal-title">添加分类</h4>
+					<h4 class="modal-title">添加导航栏</h4>
 				</div>
 				<div class="modal-body">
 					<div class="form-group">

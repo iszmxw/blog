186a855c8dc58790d28f8dd4156ced48ef45a53c
mail@54zm.com <mail@54zm.com>
2018-10-23 20:48:32 +0800

    '添加导航栏'

--- resources/views/admin/navbar_list.blade.php
+++ resources/views/admin/navbar_list.blade.php
@@ -111,5 +111,120 @@
 	</div>
 </div>
 @include('admin.public.common_js')
+<!-- Sweet alert -->
+<script src="{{asset('style/admin/inspinia/js/plugins/sweetalert/sweetalert.min.js')}}"></script>
+<script>
+    //显示添加数据框
+    function add_alert(){
+        $("#add_data").modal();
+    }
+    //添加分类
+    function add_data(){
+        var target = $("#post_url");
+        var url = target.attr("action");
+        var data = target.serialize();
+        $.post(url, data, function (json) {
+            if(json.status == 1) {
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.reload();
+                });
+            }else{
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定"
+                });
+            }
+        });
+    }
+
+    //获取分类数据
+    function EditData(id){
+        var url = "{{url('admin/ajax/category_data')}}";
+        var data = {'_token':"{{csrf_token()}}",'id':id};
+        $.post(url,data,function(json){
+            if (json.status == '1'){
+                console.log(json.data);
+                $("#sid").val(json.data.sid);
+                $("#sortname").val(json.data.sortname);
+                $("#alias").val(json.data.alias);
+                $("#pid").find("option[value="+json.data.pid+"]").attr("selected",true);
+                $("#description").val(json.data.description);
+                $("#myModal").modal();
+            }else{
+                swal("失败", json.data, "error");
+                setInterval(function(){
+                    window.location.reload();
+                },1500);
+            }
+        });
+    }
+    //保存数据
+    function SaveData(){
+        var target = $("#currentForm");
+        var url = target.attr("action");
+        var data = target.serialize();
+        $.post(url, data, function (json) {
+            if(json.status == 1) {
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定",
+                },function(){
+                    window.location.reload();
+                });
+            }else{
+                swal({
+                    title: "提示信息",
+                    text: json.data,
+                    confirmButtonColor: "#DD6B55",
+                    confirmButtonText: "确定"
+                });
+            }
+        });
+    }
+    //删除友情链接数据
+    function deleted(id){
+        var url = "{{url('admin/ajax/category_delete_check')}}";
+        var data = {'_token':'{{csrf_token()}}','id':id};
+        swal({
+                title: "你确定？",
+                text: "你将无法恢复这条数据！",
+                type: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#DD6B55",
+                confirmButtonText: "删除",
+                cancelButtonText: "取消",
+                closeOnConfirm: false,
+                closeOnCancel: false
+            },
+            function (isConfirm) {
+                if (isConfirm) {
+                    $.post(url,data,function(json){
+                        if (json.status == '1'){
+                            swal("删除", "您的分类已被删除", "success");
+                            setInterval(function(){
+                                window.location.reload();
+                            },1500);
+                        }else{
+                            swal("操作失败", "删除失败请稍后再试！", "error");
+                        }
+                    });
+                } else {
+                    swal("取消", "您已取消了删除", "error");
+                    setInterval(function(){
+                        window.location.reload();
+                    },1500);
+                }
+            });
+    }
+</script>
 </body>
 </html>

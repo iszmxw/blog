60ea19adb50abaca84229b7979d33506e3453715
mail@54zm.com <mail@54zm.com>
2018-10-23 20:50:24 +0800

    '添加导航栏'

--- resources/views/admin/navbar_list.blade.php
+++ resources/views/admin/navbar_list.blade.php
@@ -110,6 +110,90 @@
 		@include('admin.public.footer')
 	</div>
 </div>
+{{--添加数据框--}}
+<div class="modal inmodal fade" id="add_data" tabindex="-1" role="dialog"  aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<form action="{{url('admin/ajax/category_add_check')}}" id="post_url">
+				<input type="hidden" name="_token" value="{{csrf_token()}}">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+					<h4 class="modal-title">添加分类</h4>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+						<label>分类名称</label>
+						<input type="text" placeholder="分类名称" name="sortname" class="form-control">
+					</div>
+					<div class="form-group">
+						<label>别名</label>
+						<input type="text" placeholder="别名" name="alias" class="form-control">
+					</div>
+					<div class="form-group">
+						<label>上级分类</label>
+						<select class="input-sm form-control input-s-sm inline" name="pid">
+							<option value="0">无父级</option>
+							@foreach($sort as $value)
+								<option value="{{$value['sid']}}">{{$value['sortname']}}</option>
+							@endforeach
+						</select>
+					</div>
+					<div class="form-group">
+						<label>分类描述</label>
+						<input type="text" placeholder="分类描述" name="description" class="form-control">
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-white" data-dismiss="modal">关闭</button>
+					<button type="button" class="btn btn-primary" onclick="add_data()">添加数据</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
+{{--编辑数据框--}}
+<div class="modal inmodal fade" id="myModal" tabindex="-1" role="dialog"  aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<form action="{{url('admin/ajax/category_data_edit_check')}}" id="currentForm">
+				<input type="hidden" name="sid" id="sid">
+				<input type="hidden" name="_token" value="{{csrf_token()}}">
+				<div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+					<h4 class="modal-title">编辑分类</h4>
+				</div>
+				<div class="modal-body">
+					<div class="form-group">
+						<label>分类名称</label>
+						<input type="text" placeholder="分类名称" name="sortname" id="sortname" class="form-control">
+					</div>
+					<div class="form-group">
+						<label>别名</label>
+						<input type="text" placeholder="别名" name="alias" id="alias" class="form-control">
+					</div>
+					<div class="form-group">
+						<label>上级分类</label>
+						<select class="input-sm form-control input-s-sm inline" name="pid" id="pid">
+							<option value="0">无父级</option>
+							@foreach($sort as $value)
+								<option value="{{$value['sid']}}">{{$value['sortname']}}</option>
+							@endforeach
+						</select>
+					</div>
+					<div class="form-group">
+						<label>分类描述</label>
+						<input type="text" placeholder="分类描述" name="description" id="description" class="form-control">
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-white" data-dismiss="modal">关闭</button>
+					<button type="button" class="btn btn-primary" onclick="SaveData()">保存更改</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
 @include('admin.public.common_js')
 <!-- Sweet alert -->
 <script src="{{asset('style/admin/inspinia/js/plugins/sweetalert/sweetalert.min.js')}}"></script>

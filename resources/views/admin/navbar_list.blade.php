2cee28adaf42d85b596a8d8184454128afeb430b
mail@54zm.com <mail@54zm.com>
2018-10-25 17:23:54 +0800

    '添加导航栏'

--- resources/views/admin/navbar_list.blade.php
+++ resources/views/admin/navbar_list.blade.php
@@ -124,16 +124,16 @@
 				</div>
 				<div class="modal-body">
 					<div class="form-group">
-						<label class="col-sm-2 control-label">导航栏名称</label>
+						<label>导航栏名称</label>
 						<input type="text" placeholder="导航栏名称" name="naviname" class="form-control">
 					</div>
 					<div class="form-group">
-						<label class="col-sm-2 control-label">地址</label>
+						<label>地址</label>
 						<input type="text" placeholder="地址" name="url" class="form-control">
 					</div>
 
 					<div class="form-group">
-						<label class="col-sm-2 control-label">是否显示</label>
+						<label>是否显示</label>
 						<div class="col-sm-10">
 							<div class="i-checks"><label> <input type="radio" checked="" value="option2" name="a"> <i></i> 显示 </label></div>
 							<div class="i-checks"><label> <input type="radio" value="option1" name="a"> <i></i> 隐藏 </label></div>
@@ -141,7 +141,7 @@
 					</div>
 
 					<div class="form-group">
-						<label class="col-sm-2 control-label">是否新窗口打开</label>
+						<label>是否新窗口打开</label>
 						<select class="input-sm form-control input-s-sm inline" name="newtab">
 							<option value="n">否</option>
 							<option value="y">是</option>

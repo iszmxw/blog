b19d48bf5f9df2559d908f57483da3396fd83164
mail@54zm.com <mail@54zm.com>
2018-10-26 12:36:36 +0800

    '添加导航栏'

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -102,6 +102,7 @@ class CategoryController extends Controller
         $hide = $request->get('hide');
         $newtab = $request->get('newtab');
         if(!$naviname)return response()->json(['data'=>'请输入导航栏名称','status'=>'0']);
+        if(!$url)return response()->json(['data'=>'请输入导航栏名称','status'=>'0']);
         $data['naviname'] = $naviname;
         $data['url'] = $url;
         $data['hide'] = $hide;

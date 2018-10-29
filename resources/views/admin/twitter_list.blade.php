393664da3663e0a9bc4364a3d2ba6daa81a5d7aa
iszmxw <mail@54zm.com>
2018-10-29 18:10:32 +0800

    '评论管理'

--- resources/views/admin/twitter_list.blade.php
+++ resources/views/admin/twitter_list.blade.php
@@ -42,8 +42,9 @@
 									</div>
 
 									<div class="vertical-timeline-content">
-										<h2><a href="JavaScript:;" class="btn btn-sm btn-primary"> 追梦小窝</a></h2>
+										<h2>追梦小窝</h2>
 										<p>{{$value['content']}}</p>
+										<a href="JavaScript:;" class="btn btn-sm btn-primary"> 更多信息</a>
 										<span class="vertical-date">
                                         时间 <br/>
                                         <small>{{date('Y-m-d H:i:s',$value['date'])}}</small>

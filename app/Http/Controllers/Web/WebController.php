0b17f05041022df706e5664560b520115ffa6d5b
mail@54zm.com <mail@54zm.com>
2018-11-01 14:30:55 +0800

    '前台页面'

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -14,8 +14,9 @@ class WebController extends Controller
         $nav = $request->get('nav');
         $blog = Blog::getPaginate([],['title','date','content','views'],'date','DESC',10);
         $blog['date'] = date('Y-m-d H:i:s',$blog['date']);
+        $blog['content'] = substr($blog['content'],0,200);
         $data = ['nav'=>$nav,'blog'=>$blog];
-        dump($data);
+//        dump($data);
         return view('web.default_template.index',$data);
     }
 

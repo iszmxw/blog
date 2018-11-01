216af8b2676bc83811a43e99be91c7f0881cecc0
mail@54zm.com <mail@54zm.com>
2018-11-01 16:31:02 +0800

    '前台页面'

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -34,7 +34,7 @@ class WebController extends Controller
         $blog['sortname'] = Sort::getValue(['sid'=>$blog['sortid']],'sortname');
         $where = '"gid","like","%".",{$blog[\'gid\']}"."%"';
         dump($where);
-        $blog['tags'] = Tag::where('gid','like','%'.$blog['gid'].'%')->get();
+        $blog['tags'] = Tag::getList(`gid`,`like`,`%`.$blog['gid'].`%`);
         dump($blog['tags']);
         $data = ['nav'=>$nav,'blog'=>$blog];
         return view('web.default_template.article',$data);

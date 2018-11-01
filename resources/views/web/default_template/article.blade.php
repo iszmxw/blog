ccf83f16e060248a52dd8bd2495e2166302165ca
mail@54zm.com <mail@54zm.com>
2018-11-01 16:41:23 +0800

    '前台页面'

--- resources/views/web/default_template/article.blade.php
+++ resources/views/web/default_template/article.blade.php
@@ -41,7 +41,7 @@
             </div>
             <div class="tags">
                 @foreach($blog['tags'] as $value)
-                <a href="{{url('/tag/').$value['tid']}}" target="_blank">{{$value['tagname']}}</a>
+                <a href="{{url('/tag').'/'.$value['tid']}}" target="_blank">{{$value['tagname']}}</a>
                 @endforeach
             </div>
             <div class="news_about">

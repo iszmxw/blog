75817a5446b7dcdbe46035440892c217a507d9b0
mail@54zm.com <mail@54zm.com>
2018-11-01 13:06:20 +0800

    '前台页面'

--- resources/views/web/default_template/public/header.blade.php
+++ resources/views/web/default_template/public/header.blade.php
@@ -2,25 +2,17 @@
 	<div id="mnav">
 		<h2><span class="navicon"></span></h2>
 		<ul>
-			<li><a href="index.html">网站首页</a></li>
-			<li><a href="about.html">关于我</a></li>
-			<li><a href="share.html">模板分享</a></li>
-			<li><a href="list.html">学无止境</a></li>
-			<li><a href="list.html">慢生活</a></li>
-			<li><a href="link.html">博客导航</a></li>
-			<li><a href="gbook.html">留言</a></li>
+			@foreach($nav as $value)
+			<li><a href="{{$value['url']}}">{{$value['naviname']}}</a></li>
+			@endforeach
 		</ul>
 	</div>
 	<div class="topnav">
 		<nav>
 			<ul>
-				<li><a href="index.html">网站首页</a></li>
-				<li><a href="about.html">关于我</a></li>
-				<li><a href="share.html">模板分享</a></li>
-				<li><a href="list.html">学无止境</a></li>
-				<li><a href="list.html">慢生活</a></li>
-				<li><a href="link.html">博客导航</a></li>
-				<li><a href="gbook.html">留言</a></li>
+				@foreach($nav as $value)
+					<li><a href="{{$value['url']}}">{{$value['naviname']}}</a></li>
+				@endforeach
 			</ul>
 		</nav>
 	</div>

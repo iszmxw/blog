12c7b6f4429e8ab03373dcf1fc365ebf9f72da41
mail@54zm.com <mail@54zm.com>
2018-11-01 13:11:28 +0800

    '前台页面'

--- resources/views/web/default_template/public/header.blade.php
+++ resources/views/web/default_template/public/header.blade.php
@@ -3,7 +3,7 @@
 		<h2><span class="navicon"></span></h2>
 		<ul>
 			@foreach($nav as $value)
-			<li><a href="{{$value['url']}}">{{$value['naviname']}}</a></li>
+			<li><a href="{{$value['url']}}" @if($value['newtab'] == 'y')target="_blank"@endif>{{$value['naviname']}}</a></li>
 			@endforeach
 		</ul>
 	</div>

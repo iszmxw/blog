89405ee4335d672d97858ec31eafad57e3eb6fdb
mail@54zm.com <mail@54zm.com>
2018-11-01 14:57:30 +0800

    '前台页面'

--- app/Http/Middleware/Web/Web.php
+++ app/Http/Middleware/Web/Web.php
@@ -19,7 +19,7 @@ class Web
         $route = $request->getPathInfo();
         switch ($route){
             case '/';
-            case 'article';
+            case 'article/{id}';
                 self::Navdata($request);
                 break;
         }

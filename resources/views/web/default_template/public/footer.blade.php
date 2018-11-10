5e1aaa9ee9855c2c7ebdfe07c47954ff09cae54e
mail@54zm.com <mail@54zm.com>
2018-11-10 17:42:26 +0800

    'mb'

--- resources/views/web/default_template/public/footer.blade.php
+++ resources/views/web/default_template/public/footer.blade.php
@@ -1,3 +1,8 @@
-<footer>
-	<p>Design by <a href="/">追梦小窝的博客</a> <a href="/">蜀ICP备11002373号-1</a></p>
-</footer>
+<div class="footer">
+	<div class="pull-right">
+		<strong>blog.54zm..com</strong>
+	</div>
+	<div>
+		<strong>Copyright</strong> 追梦小窝 &copy; 2017-2018
+	</div>
+</div>
--- resources/views/web/default_template/public/header.blade.php
+++ resources/views/web/default_template/public/header.blade.php
@@ -1,19 +1,56 @@
-<header>
-	<div id="mnav">
-		<h2><span class="navicon"></span></h2>
-		<ul>
-			@foreach($nav as $value)
-			<li><a href="{{$value['url']}}" @if($value['newtab'] == 'y')target="_blank"@endif>{{$value['naviname']}}</a></li>
-			@endforeach
+<ul class="nav metismenu" id="side-menu">
+	<li class="nav-header">
+		<div class="dropdown profile-element">
+                        <span>
+                            <img alt="image" class="img-circle" src="img/profile_small.jpg" />
+                        </span>
+		</div>
+		<div class="logo-element">
+			博客
+		</div>
+	</li>
+	<li>
+		<a href="index.html">
+			<i class="fa fa-th-large"></i>
+			<span class="nav-label">首页</span>
+		</a>
+	</li>
+	<li>
+		<a href="index.html">
+			<i class="fa fa-th-large"></i>
+			<span class="nav-label">软件分享</span>
+		</a>
+	</li>
+	<li>
+		<a href="index.html">
+			<i class="fa fa-th-large"></i>
+			<span class="nav-label">随笔日记</span>
+		</a>
+	</li>
+	<li>
+		<a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">WEB类</span> <span class="fa arrow"></span></a>
+		<ul class="nav nav-second-level">
+			<li><a href="index.html">插件软件</a></li>
+			<li><a href="dashboard_2.html">WEB前端</a></li>
+			<li><a href="dashboard_3.html">WEB程序</a></li>
 		</ul>
-	</div>
-	<div class="topnav">
-		<nav>
-			<ul>
-				@foreach($nav as $value)
-					<li><a href="{{$value['url']}}" @if($value['newtab'] == 'y')target="_blank"@endif>{{$value['naviname']}}</a></li>
-				@endforeach
-			</ul>
-		</nav>
-	</div>
-</header>
+	</li>
+	<li>
+		<a href="index.html">
+			<i class="fa fa-th-large"></i>
+			<span class="nav-label">说说</span>
+		</a>
+	</li>
+	<li>
+		<a href="index.html">
+			<i class="fa fa-th-large"></i>
+			<span class="nav-label">学习空间</span>
+		</a>
+	</li>
+	<li>
+		<a href="index.html">
+			<i class="fa fa-th-large"></i>
+			<span class="nav-label">登录</span>
+		</a>
+	</li>
+</ul>

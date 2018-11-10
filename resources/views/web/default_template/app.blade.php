e54cc0d2ab1575a7f126c9db326803c7be574f95
mail@54zm.com <mail@54zm.com>
2018-11-10 17:46:27 +0800

    'mb'

--- resources/views/web/default_template/app.blade.php
+++ resources/views/web/default_template/app.blade.php
@@ -8,7 +8,7 @@
     <link href="{{asset('style/web/default_template/font-awesome/css/font-awesome.css')}}" rel="stylesheet">
     <link href="{{asset('style/web/default_template/css/animate.css')}}" rel="stylesheet">
     <link href="{{asset('style/web/default_template/css/style.css')}}" rel="stylesheet">
-
+    @yield('style')
 </head>
 <body class="">
 <div id="wrapper">
@@ -48,14 +48,14 @@
 </div>
 
 <!-- Mainly scripts -->
-<script src="js/jquery-3.1.1.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/plugins/metisMenu/jquery.metisMenu.js"></script>
-<script src="js/plugins/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="{{asset('style/web/default_template/js/jquery-3.1.1.min.js')}}"></script>
+<script src="{{asset('style/web/default_template/js/bootstrap.min.js')}}"></script>
+<script src="{{asset('style/web/default_template/js/plugins/metisMenu/jquery.metisMenu.js')}}"></script>
+<script src="{{asset('style/web/default_template/js/plugins/slimscroll/jquery.slimscroll.min.js')}}"></script>
 
 <!-- Custom and plugin javascript -->
-<script src="js/inspinia.js"></script>
-<script src="js/plugins/pace/pace.min.js"></script>
-
+<script src="{{asset('style/web/default_template/js/inspinia.js')}}"></script>
+<script src="{{asset('style/web/default_template/js/plugins/pace/pace.min.js')}}"></script>
+@yield('script')
 </body>
 </html>

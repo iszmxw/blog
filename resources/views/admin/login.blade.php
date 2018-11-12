ca4e4621de527c63fde14c24995bf302193609d3
mail@54zm.com <mail@54zm.com>
2018-11-12 12:26:48 +0800

    'QQ登录'

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -78,7 +78,7 @@
         //以下为按钮点击事件的逻辑。注意这里要重新打开窗口
         //否则后面跳转到QQ登录，授权页面时会直接缩小当前浏览器的窗口，而不是打开新窗口
         var A=window.open(
-            "qq_login",
+            "qq_login_auth",
 			"TencentLogin",
             "width=450,height=320,menubar=0,scrollbars=1,resizable=1,status=1,titlebar=0,toolbar=0,location=1"
 		);

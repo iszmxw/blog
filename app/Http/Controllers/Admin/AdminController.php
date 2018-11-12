4b793b88a06651d60f02f8cce41f089d4a1510b1
mail@54zm.com <mail@54zm.com>
2018-11-12 12:18:29 +0800

    'QQ登录'

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -90,8 +90,8 @@ class AdminController extends Controller
     //QQ登录
     public function qq_login(Request $request)
     {
-        $appid = '101454284';
-        $appkey = 'f21c00281b7411a01962167c734a4ec1';
+        $appid = '101523010';
+        $appkey = 'ac7d4e7a120f907e69df29799619cc47';
         $redirect_uri = 'http://blog.54zm.cn/admin/qq_login';
         $url='https://graph.qq.com/oauth2.0/authorize?response_type=code&client_id='.$appid.'&redirect_uri='.$redirect_uri.'&state=state';
         $response = HttpCurl::doGet($url);

29e5c70a59080c967d7a675923ce54c79079ba9e
mail@54zm.com <mail@54zm.com>
2018-11-13 19:41:38 +0800

    '首页'

--- resources/views/web/default_template/index.blade.php
+++ resources/views/web/default_template/index.blade.php
@@ -12,7 +12,7 @@
         @foreach($blog as $key=>$val)
         <div class="ibox">
             <div class="ibox-content">
-                <a href="{{url('article/').$val['gid']}}" class="btn-link">
+                <a href="{{url('article')}}/{{$val['gid']}}" class="btn-link">
                     <h2>
                         {{$val['title']}}
                     </h2>

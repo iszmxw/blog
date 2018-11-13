261f9b71a22dde4bf60e5d4d313cd754ee1134ba
mail@54zm.com <mail@54zm.com>
2018-11-13 20:21:21 +0800

    '首页'

--- resources/views/web/default_template/app.blade.php
+++ resources/views/web/default_template/app.blade.php
@@ -44,20 +44,7 @@
         </div>
 
         <div class="row wrapper border-bottom white-bg page-heading">
-            <div class="col-lg-10">
-                <h2>博客</h2>
-                <ol class="breadcrumb">
-                    <li>
-                        <a href="{{url('/')}}">主页</a>
-                    </li>
-                    <li class="active">
-                        <strong>博客</strong>
-                    </li>
-                </ol>
-            </div>
-            <div class="col-lg-2">
-
-            </div>
+            @yield('header_bar')
         </div>
 
         <div class="wrapper wrapper-content  animated fadeInRight blog">

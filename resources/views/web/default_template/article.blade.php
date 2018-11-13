1335e434d905b2503fff77b7a045d0ef6940b095
mail@54zm.com <mail@54zm.com>
2018-11-13 22:56:39 +0800

    '首页'

--- resources/views/web/default_template/article.blade.php
+++ resources/views/web/default_template/article.blade.php
@@ -21,7 +21,7 @@
     </div>
     <div class="col-sm-8">
         <div class="title-action">
-            <a href="{{url()->previous()}}" class="btn btn-primary">这是行动区</a>
+            <a href="{{url()->previous()}}" class="btn btn-primary">返回上一级</a>
         </div>
     </div>
 @endsection

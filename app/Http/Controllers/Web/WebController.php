ccc0964a63d42a0c7eb92d69e4c558e07aa5557b
mail@54zm.com <mail@54zm.com>
2018-11-13 18:28:59 +0800

    '前台页面'

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -17,9 +17,48 @@ class WebController extends Controller
         $content = str_replace('继续阅读&gt;&gt;', '', $content);
         $content = preg_replace("/\[hide\](.*)\[\/hide\]/Uims", '|*********此处内容回复可见*********|', strip_tags($content));
         if ($strlen) {
-            $content = subString($content, 0, $strlen);
+            $content = $this->subString($content, 0, $strlen);
         }
         return $content;
+    }
+    /**
+     * 截取编码为utf8的字符串
+     *
+     * @param string $strings 预处理字符串
+     * @param int $start 开始处 eg:0
+     * @param int $length 截取长度
+     */
+    function subString($strings, $start, $length) {
+        if (function_exists('mb_substr') && function_exists('mb_strlen')) {
+            $sub_str = mb_substr($strings, $start, $length, 'utf8');
+            return mb_strlen($sub_str, 'utf8') < mb_strlen($strings, 'utf8') ? $sub_str . '...' : $sub_str;
+        }
+        $str = substr($strings, $start, $length);
+        $char = 0;
+        for ($i = 0; $i < strlen($str); $i++) {
+            if (ord($str[$i]) >= 128)
+                $char++;
+        }
+        $str2 = substr($strings, $start, $length + 1);
+        $str3 = substr($strings, $start, $length + 2);
+        if ($char % 3 == 1) {
+            if ($length <= strlen($strings)) {
+                $str3 = $str3 .= '...';
+            }
+            return $str3;
+        }
+        if ($char % 3 == 2) {
+            if ($length <= strlen($strings)) {
+                $str2 = $str2 .= '...';
+            }
+            return $str2;
+        }
+        if ($char % 3 == 0) {
+            if ($length <= strlen($strings)) {
+                $str = $str .= '...';
+            }
+            return $str;
+        }
     }
     //首页
     public function index(Request $request)

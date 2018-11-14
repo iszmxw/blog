2beee4bf0cfe936fd6ec6a10ffd71b0dca8506b8
mail@54zm.com <mail@54zm.com>
2018-11-14 22:11:35 +0800

    '首页'

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -11,6 +11,7 @@ use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 
 class WebController extends Controller
 {

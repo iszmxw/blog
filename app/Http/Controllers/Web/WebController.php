fc73d305377e6ca9ea30250bddf1583f69ccc0b9
mail@54zm.com <mail@54zm.com>
2018-11-14 22:43:51 +0800

    '首页'

--- app/Http/Controllers/Web/WebController.php
+++ app/Http/Controllers/Web/WebController.php
@@ -18,6 +18,7 @@ class WebController extends Controller
     //首页
     public function index(Request $request)
     {
+        dump(md5('442246396'));
         dump(md5('101523010442246396'));
         $nav = $request->get('nav');
         $blog = Blog::getPaginate([],['gid','sortid','title','date','content','views'],'date','DESC',10);

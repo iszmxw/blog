bab954addeb16fc8c784f62af90856f35caef5c7
mail@54zm.com <mail@54zm.com>
2018-11-15 08:52:42 +0800

    'qqlogin'

--- app/Http/Middleware/Web/Web.php
+++ app/Http/Middleware/Web/Web.php
@@ -73,7 +73,6 @@ class Web
     //格式化返回值
     public static function format_response($re, Closure $next)
     {
-        dd($re);
         if ($re['status'] == '0') {
             return $re['data'];
         } else {

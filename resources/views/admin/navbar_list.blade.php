e9d84a8371789a942b8d94dedaa8864e466a63b2
mail@54zm.com <mail@54zm.com>
2018-11-17 18:15:10 +0800

    'blog'

--- resources/views/admin/navbar_list.blade.php
+++ resources/views/admin/navbar_list.blade.php
@@ -173,8 +173,28 @@
 						<input type="text" placeholder="导航栏名称" name="naviname"  id="naviname" class="form-control">
 					</div>
 					<div class="form-group">
-						<label class="control-label">地址</label>
-						<input type="text" placeholder="地址" name="url" id="url" class="form-control">
+						<div class="tabs-container">
+							<ul class="nav nav-tabs">
+								<li class="active"><a data-toggle="tab" href="#tab-1"> 系统地址</a></li>
+								<li class=""><a data-toggle="tab" href="#tab-2">手动输入</a></li>
+							</ul>
+							<div class="tab-content">
+								<div id="tab-1" class="tab-pane active">
+									<div class="panel-body">
+										<label class="control-label">地址</label>
+										<input type="text" placeholder="地址" name="url" id="url" class="form-control">
+									</div>
+								</div>
+								<div id="tab-2" class="tab-pane">
+									<div class="panel-body">
+										<label class="control-label">地址</label>
+										<input type="text" placeholder="地址" name="url" id="url" class="form-control">
+									</div>
+								</div>
+							</div>
+
+
+						</div>
 					</div>
 					<div class="form-group">
 						<label class="control-label">是否显示</label>

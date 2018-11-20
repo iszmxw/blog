dbf0306236d4618cdca2f287500e4e9f21a4f98c
mail@54zm.com <mail@54zm.com>
2018-11-20 21:51:15 +0800

    'api'

--- resources/views/admin/navbar_list.blade.php
+++ resources/views/admin/navbar_list.blade.php
@@ -128,8 +128,33 @@
 						<input type="text" placeholder="导航栏名称" name="naviname" class="form-control">
 					</div>
 					<div class="form-group">
-						<label class="control-label">地址</label>
-						<input type="text" placeholder="地址" name="url" class="form-control">
+						<div class="tabs-container">
+							<ul class="nav nav-tabs">
+								<li class="active"><a data-toggle="tab" href="#tab-1"><i class="fa fa-cog"></i>&nbsp;系统地址</a></li>
+								<li class=""><a data-toggle="tab" href="#tab-2"><i class="fa fa-link"></i>&nbsp;手动输入</a></li>
+							</ul>
+							<div class="tab-content">
+								<div id="tab-1" class="tab-pane active">
+									<div class="panel-body">
+										<label class="control-label">系统地址</label>
+										<select class="form-control m-b" name="url">
+											<option value="0" selected="selected">请选择</option>
+											@foreach($category as $val)
+												<option value="{{ $val['sid'] }}">{{ $val['sortname'] }}</option>
+											@endforeach
+										</select>
+									</div>
+								</div>
+								<div id="tab-2" class="tab-pane">
+									<div class="panel-body">
+										<label class="control-label">地址</label>
+										<input type="text" placeholder="地址" name="url" class="form-control">
+									</div>
+								</div>
+							</div>
+
+
+						</div>
 					</div>
 					<div class="form-group">
 						<label class="control-label">是否显示</label>

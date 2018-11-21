cec5cfeaeb35c6625c12ca4863d9437ac1ab8bc3
mail@54zm.com <mail@54zm.com>
2018-11-21 12:17:41 +0800

    '记录用户的来源地址'

--- app/Library/IpAddress.php
+++ app/Library/IpAddress.php
@@ -9,7 +9,7 @@ class IpAddress
     public static function address($ip)
     {
         $url = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query={$ip}&co=&resource_id=6006&t=1542771237972&ie=utf8&oe=gbk";
-        $response = HttpCurl::httpRequest($url,'GET',[],['Content-Type: application/json;charset=gbk']);
+        $response = HttpCurl::httpRequest($url,'GET',[],['Content-Type: application/json;charset=GBK']);
 //        $re = json_decode($response,JSON_UNESCAPED_UNICODE);
         return $response;
     }

923e27365acefd2cb66e575b33826b2e5ceccfc1
mail@54zm.com <mail@54zm.com>
2018-11-21 11:31:39 +0800

    '记录用户的来源地址'

--- app/Library/IpAddress.php
+++ app/Library/IpAddress.php
@@ -11,6 +11,6 @@ class IpAddress
         $url = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query={$ip}&resource_id=6006";
         $response = HttpCurl::doGet($url);
         $re = json_decode($response,true);
-        return $re;
+        return $response;
     }
 }

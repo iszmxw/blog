21d841065cae3ecec385a43e4f1c498ed2e51a09
mail@54zm.com <mail@54zm.com>
2018-11-21 18:21:16 +0800

    '记录用户的来源地址'

--- public/style/admin/js/article_add.js
+++ public/style/admin/js/article_add.js
@@ -13,23 +13,19 @@ function add_data(){
     var summernote = $(".summernote").summernote('code');
     data = data +'&'+'content='+summernote;
     $.post(url, data, function (json) {
-        console.log(data);
-        console.log(json);
-        // if (json.status == -1) {
-        //     window.location.reload();
-        // } else if(json.status == 1) {
-        //     console.log(data);
-        //     console.log(json);
-        //     toastr.success(json.data);
-        //     // setInterval(function(){
-        //     //     window.location.reload();
-        //     // },3000);
-        //     return false;
-        // }else{
-        //     // toastr.error(json.data);
-        //     // setInterval(function(){
-        //     //     window.location.reload();
-        //     // },3000);
-        // }
+        if (json.status == -1) {
+            window.location.reload();
+        } else if(json.status == 1) {
+            toastr.success(json.data);
+            setInterval(function(){
+                window.location.reload();
+            },3000);
+            return false;
+        }else{
+            toastr.error(json.data);
+            setInterval(function(){
+                window.location.reload();
+            },3000);
+        }
     });
 }

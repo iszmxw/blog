0cde8b9e51ee4a2e15d15ac26a4f06285c0d7f66
mail@54zm.com <mail@54zm.com>
2018-11-21 12:23:50 +0800

    '记录用户的来源地址'

--- app/Library/IpAddress.php
+++ app/Library/IpAddress.php
@@ -12,6 +12,6 @@ class IpAddress
         $response = HttpCurl::doGet($url);
         $response = mb_convert_encoding($response, 'utf-8','GB2312');
         $re = json_decode($response,JSON_UNESCAPED_UNICODE);
-        return $re;
+        return $re['data']['0'];
     }
 }

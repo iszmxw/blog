38582556e1dca69bf2c4c51c453099340368f6e7
mail@54zm.com <mail@54zm.com>
2018-11-21 18:24:41 +0800

    '记录用户的来源地址'

--- public/style/admin/js/article_add.js
+++ public/style/admin/js/article_add.js
@@ -6,26 +6,45 @@ $(document).ready(function () {
         focus: true
     });
 });
+$.fn.serializeObject = function() {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
+
 function add_data(){
     var target = $("#post_url");
     var url = target.attr("action");
     var data = target.serialize();
+    var datas = target.serializeObject();
+    console.log(datas);
     var summernote = $(".summernote").summernote('code');
     data = data +'&'+'content='+summernote;
-    $.post(url, data, function (json) {
-        if (json.status == -1) {
-            window.location.reload();
-        } else if(json.status == 1) {
-            toastr.success(json.data);
-            setInterval(function(){
-                window.location.reload();
-            },3000);
-            return false;
-        }else{
-            toastr.error(json.data);
-            setInterval(function(){
-                window.location.reload();
-            },3000);
-        }
-    });
+    // $.post(url, data, function (json) {
+    //     if (json.status == -1) {
+    //         window.location.reload();
+    //     } else if(json.status == 1) {
+    //         toastr.success(json.data);
+    //         setInterval(function(){
+    //             window.location.reload();
+    //         },3000);
+    //         return false;
+    //     }else{
+    //         toastr.error(json.data);
+    //         setInterval(function(){
+    //             window.location.reload();
+    //         },3000);
+    //     }
+    // });
 }

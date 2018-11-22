cfbf55959d4b2b5927dcada467e0ac9426c6e2d9
mail@54zm.com <mail@54zm.com>
2018-11-22 12:28:09 +0800

    '优化后台'

--- config/database.php
+++ config/database.php
@@ -112,13 +112,13 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
+            'database' => env('REDIS_DB', 1),
         ],
         'blog_admin' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 0),
+            'database' => env('REDIS_DB', 2),
         ],
 
         'cache' => [

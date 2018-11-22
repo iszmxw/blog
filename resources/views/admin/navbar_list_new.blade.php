9bcf193317c037b50838d689556af5859ad8ace6
iszmxw <mail@54zm.com>
2018-11-22 22:57:40 +0800

    '导航栏修改'

--- resources/views/admin/navbar_list_new.blade.php
+++ resources/views/admin/navbar_list_new.blade.php
@@ -54,8 +54,8 @@
 									<li class="dd-item" data-id="{{ $val['id'] }}">
 										<div class="dd-handle">
 											<span class="pull-right">
-												<button class="btn btn-xs btn-info" type="button" onclick="EditData('{{$val['id']}}')"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑</button>
-												<button class="btn btn-xs btn-danger" type="button" onclick="deleted('{{$val['id']}}')"><i class="fa fa-times"></i>&nbsp;&nbsp;删除</button>
+												<button style="position:absolute" class="btn btn-xs btn-info" type="button" onclick="EditData('{{$val['id']}}')"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑</button>
+												<button style="position:absolute" class="btn btn-xs btn-danger" type="button" onclick="deleted('{{$val['id']}}')"><i class="fa fa-times"></i>&nbsp;&nbsp;删除</button>
 											</span>
 											<span class="label label-info"><i class="fa fa-link"></i></span> {{ $val['naviname'] }}
 										</div>
@@ -65,8 +65,8 @@
 											<li class="dd-item" data-id="{{$v['id']}}">
 												<div class="dd-handle">
 													<span class="pull-right">
-														<button class="btn btn-xs btn-info" type="button" onclick="EditData('{{$v['id']}}')"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑</button>
-														<button class="btn btn-xs btn-danger" type="button" onclick="deleted('{{$v['id']}}')"><i class="fa fa-times"></i>&nbsp;&nbsp;删除</button>
+														<button style="position:absolute" class="btn btn-xs btn-info" type="button" onclick="EditData('{{$v['id']}}')"><i class="fa fa-edit"></i>&nbsp;&nbsp;编辑</button>
+														<button style="position:absolute" class="btn btn-xs btn-danger" type="button" onclick="deleted('{{$v['id']}}')"><i class="fa fa-times"></i>&nbsp;&nbsp;删除</button>
 													</span>
 													<span class="label label-info"><i class="fa fa-link"></i></span> {{ $v['naviname'] }}
 												</div>

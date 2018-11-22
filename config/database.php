b7d2df5c9193ce2106fdd139de8f74fb3c7aecdc
mail@54zm.com <mail@54zm.com>
2018-11-22 12:34:32 +0800

    '优化后台'

--- config/database.php
+++ config/database.php
@@ -118,7 +118,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
             'port' => env('REDIS_PORT', 6379),
-            'database' => env('REDIS_DB', 15),
+            'database' => env('REDIS_DB', 0),
         ],
 
         'cache' => [

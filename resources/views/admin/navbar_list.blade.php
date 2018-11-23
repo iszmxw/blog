f6dfcec1bcb77c89ba077821bb966b9fc0a8aea9
iszmxw <mail@54zm.com>
2018-11-23 12:45:03 +0800

    'navbar'

--- resources/views/admin/navbar_list.blade.php
+++ resources/views/admin/navbar_list.blade.php
@@ -279,17 +279,6 @@
     $('#nestable2').nestable({
         group: 1
     }).on('change', updateOutput);
-    $('#nestable-menu').on('click', function (e) {
-        var target = $(e.target),
-            action = target.data('action');
-        if (action === 'expand-all') {
-            $('.dd').nestable('expandAll');
-        }
-        if (action === 'collapse-all') {
-            $('.dd').nestable('collapseAll');
-        }
-    });
-
 </script>
 <script>
     //显示添加数据框

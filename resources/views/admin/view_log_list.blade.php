2b44f1e92efafc4d2864782cfab09d4f6131969b
mail@54zm.com <mail@54zm.com>
2018-11-24 23:49:42 +0800

    'log'

--- resources/views/admin/view_log_list.blade.php
+++ resources/views/admin/view_log_list.blade.php
@@ -63,9 +63,9 @@
                                         <td><span class="label label-primary">{{$value['ip_position']}}</span></td>
                                         <td><span class="label label-success">{{$value['num']}}</span></td>
                                         <td>{{$value['created_at']->diffForHumans()}}</td>
-                                        <td>@if($value['previous'])<span class="label label-default"><a href="{{$value['previous']}}" target="_blank">新窗口打开查看</a></span>@else暂无@endif</td>
+                                        <td>@if($value['previous'])<span class="label label-default"><a href="{{$value['previous']}}" title="{{$value['previous']}}" target="_blank">新窗口打开查看</a></span>@else暂无@endif</td>
                                         <td>{{$value['updated_at']->diffForHumans()}}</td>
-                                        <td><span class="label label-danger"><a href="{{$value['full']}}" target="_blank">新窗口打开查看</a></span></td>
+                                        <td><span class="label label-danger"><a href="{{$value['full']}}" title="{{$value['full']}}" target="_blank">新窗口打开查看</a></span></td>
                                     </tr>
                                     @endforeach
                                     </tbody>

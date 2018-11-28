43474020f0a6e08f0ce52fd8406eb53b9154270d
mail@54zm.com <mail@54zm.com>
2018-11-28 16:54:12 +0800

    '404'

--- vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Exceptions/views/404.blade.php
@@ -1,7 +1,7 @@
 @extends('errors::illustrated-layout')
 
 @section('code', '404')
-@section('title', __('Page Not Found'))
+@section('title', __('页面未找到'))
 
 @section('image')
 <div style="background-image: url('/svg/404.svg');" class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">

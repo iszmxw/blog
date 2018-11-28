dcfaaa7a7f60e7ce087f94b22f3315a72f54aef3
mail@54zm.com <mail@54zm.com>
2018-11-28 12:55:42 +0800

    'emlog'

--- app/Http/Middleware/Admin/Admin.php
+++ app/Http/Middleware/Admin/Admin.php
@@ -78,7 +78,7 @@ class Admin
         $data = $request->session()->get('user_data');
         if ($data){
             dump($data['photo']);
-            $data['photo'] = realpath($data['photo']);
+            $data['photo'] = realpath(base_path().$data['photo']);
             dump($data['photo']);
             $request->attributes->add(['user_data'=>$data]);
             View::share('user_data', $data);

193c770dba5da8e4746e14082ed95dd5ab1b628c
mail@54zm.com <mail@54zm.com>
2018-11-28 23:45:32 +0800

    'navi'

--- app/Http/Middleware/Admin/Admin.php
+++ app/Http/Middleware/Admin/Admin.php
@@ -22,9 +22,9 @@ class Admin
         if ($re['status'] == '0') {
             return $re['data'];
         } else {
-            switch ($route){//检测特殊路由
+            switch ($route){    //检测特殊路由
 //                case '/admin/quit';
-                    break;
+//                    break;
             }
         }
         //数据成功通过中间件，接下来传递到控制器

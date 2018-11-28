38a4d41e6c13353529a954fe0f0ca6dfd7083dea
mail@54zm.com <mail@54zm.com>
2018-11-28 23:46:11 +0800

    'navi'

--- routes/admin.php
+++ routes/admin.php
@@ -10,15 +10,12 @@
 |
 */
 
-Route::namespace('Admin')->group(function () {
+Route::middleware('admin')->namespace('Admin')->group(function () {
+    Route::get('/','AdminController@index');
     Route::get('login','AdminController@login');
     Route::get('qq_login','AdminController@qq_login');
     Route::get('qq_login_auth','AdminController@qq_login_auth');
     Route::get('quit','AdminController@quit');
-});
-
-Route::middleware('admin')->namespace('Admin')->group(function () {
-    Route::get('/','AdminController@index');
     Route::get('config','AdminController@config');
     Route::get('view_log','AdminController@view_log');
 

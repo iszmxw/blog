fee1f5125e19236905f2fe1604c6d19c6cdba124
mail@54zm.com <mail@54zm.com>
2018-11-30 23:21:04 +0800

    'nav'

--- resources/views/web/default_template/app.blade.php
+++ resources/views/web/default_template/app.blade.php
@@ -69,7 +69,7 @@
         var obj = $(".menu");
         for(var i=0;i<obj.length;i++){
             console.log(obj[i]);
-            console.log(obj[i].find('ul li'));
+            console.log(obj[i].children('li'));
         }
         // var sub_menu = obj.find('ul li').hasClass('active');
         // console.log(sub_menu);

68b9c21d61f2b2106a548a7f336412e6cf67ac0c
mail@54zm.com <mail@54zm.com>
2018-12-02 19:04:15 +0800

    'link'

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,34 +6,34 @@ class User extends Defaults
 {
     protected $table = 'user';
     protected $primaryKey = 'uid';
-    //单条数据获取
-    public static function getOne($where)
-    {
-        return self::where($where)->first();
-    }
-
-    //获取列表数据
-    public static function getList($where)
-    {
-        return self::where($where)->get();
-    }
-
-    //分页获取数据
-    public static function getPaginate($where,$sort,$desc,$paginate)
-    {
-        return self::where($where)->orderby($sort,$desc)->paginate($paginate);
-    }
-
-    //修改数据
-    public static function EditData($where,$data)
-    {
-        if($model = self::where($where)->first()){
-            foreach($data as $key=>$val){
-                $model->$key=$val;
-            }
-            $model->save();
-        }
-    }
+//    //单条数据获取
+//    public static function getOne($where)
+//    {
+//        return self::where($where)->first();
+//    }
+//
+//    //获取列表数据
+//    public static function getList($where)
+//    {
+//        return self::where($where)->get();
+//    }
+//
+//    //分页获取数据
+//    public static function getPaginate($where,$sort,$desc,$paginate)
+//    {
+//        return self::where($where)->orderby($sort,$desc)->paginate($paginate);
+//    }
+//
+//    //修改数据
+//    public static function EditData($where,$data)
+//    {
+//        if($model = self::where($where)->first()){
+//            foreach($data as $key=>$val){
+//                $model->$key=$val;
+//            }
+//            $model->save();
+//        }
+//    }
 
     //删除数据
     public static function selected_delete($where)
@@ -41,9 +41,9 @@ class User extends Defaults
         return self::where($where)->delete();
     }
 
-    //取值
-    public static function getValue($where, $value)
-    {
-        return self::where($where)->value($value);
-    }
+//    //取值
+//    public static function getValue($where, $value)
+//    {
+//        return self::where($where)->value($value);
+//    }
 }

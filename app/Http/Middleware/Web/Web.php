db8a96bf397a346b327decca3a251e27a8d5bed0
mail@54zm.com <mail@54zm.com>
2018-12-02 20:03:48 +0800

    '优化Model'

--- app/Http/Middleware/Web/Web.php
+++ app/Http/Middleware/Web/Web.php
@@ -122,10 +122,8 @@ class Web
         $log = ViewLog::getOne(['ip'=>$address['origip']]);
         if ($log){
             $num = $log['num'] + 1;
-            ViewLog::editData(['num'=>$num,'full'=>$address['full']],['id'=>$log['id']]);
-            dd(1);
+            ViewLog::editData(['id'=>$log['id']],['num'=>$num,'full'=>$address['full']]);
         }else{
-            dd(2);
             ViewLog::addData(['ip'=>$address['origip'],'ip_position'=>$address['location'],'previous'=>$address['previous'],'full'=>$address['full']]);
         }
     }

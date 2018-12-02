b7edf6be589387438db6f20a403032afa206b282
mail@54zm.com <mail@54zm.com>
2018-12-02 19:47:40 +0800

    '优化Model'

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -10,4 +10,18 @@ class Blog extends Defaults
     public $timestamps = false;
     //过滤黑名单字段
     public $guarded = [];
+
+    //分页获取数据
+    public static function getPaginate($where = [], $field = [], $paginate = 1, $orderby = "", $sort = "DESC")
+    {
+        return self::where($where)
+            ->join('sort',function($join){
+                        $join->on('sort.sid','=','blog.sortid');
+                    })
+            ->select($field)
+            ->orderby($orderby,$sort)
+            ->paginate($paginate);
+    }
+
+
 }

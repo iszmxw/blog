1e3712ef7c2f8afaba06ed733de64b1c9e349b7f
mail@54zm.com <mail@54zm.com>
2018-12-02 19:02:30 +0800

    'link'

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -10,27 +10,27 @@ class Link extends Defaults
     public $timestamps = false;
     //过滤黑名单字段
     public $guarded = [];
-    //单条数据获取
-    public static function getOne($where)
-    {
-        return self::where($where)->first();
-    }
-
-    //获取列表数据
-    public static function getList($where)
-    {
-        return self::where($where)->get();
-    }
-    //修改数据
-    public static function EditData($where,$data)
-    {
-        if($model = self::where($where)->first()){
-            foreach($data as $key=>$val){
-                $model->$key=$val;
-            }
-            $model->save();
-        }
-    }
+//    //单条数据获取
+//    public static function getOne($where)
+//    {
+//        return self::where($where)->first();
+//    }
+//
+//    //获取列表数据
+//    public static function getList($where)
+//    {
+//        return self::where($where)->get();
+//    }
+//    //修改数据
+//    public static function EditData($where,$data)
+//    {
+//        if($model = self::where($where)->first()){
+//            foreach($data as $key=>$val){
+//                $model->$key=$val;
+//            }
+//            $model->save();
+//        }
+//    }
 
     //删除数据
     public static function selected_delete($where)

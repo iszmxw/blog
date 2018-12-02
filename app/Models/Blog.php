a0ef4d6878aa50e8fdff86002a777de6ef2c0a4b
mail@54zm.com <mail@54zm.com>
2018-12-02 19:00:35 +0800

    'link'

--- app/Models/Blog.php
+++ app/Models/Blog.php
@@ -10,17 +10,17 @@ class Blog extends Defaults
     public $timestamps = false;
     //过滤黑名单字段
     public $guarded = [];
-    //单条数据获取
-    public static function getOne($where)
-    {
-        return self::where($where)->first();
-    }
-
-    //获取列表数据
-    public static function getList($where)
-    {
-        return self::where($where)->get();
-    }
+//    //单条数据获取
+//    public static function getOne($where)
+//    {
+//        return self::where($where)->first();
+//    }
+//
+//    //获取列表数据
+//    public static function getList($where)
+//    {
+//        return self::where($where)->get();
+//    }
 
     //添加数据
     public static function AddData($data)
@@ -28,28 +28,28 @@ class Blog extends Defaults
         return self::create($data);
     }
 
-    //修改数据
-    public static function EditData($where,$data)
-    {
-        if($model = self::where($where)->first()){
-            foreach($data as $key=>$val){
-                $model->$key=$val;
-            }
-            $model->save();
-        }
-    }
+//    //修改数据
+//    public static function EditData($where,$data)
+//    {
+//        if($model = self::where($where)->first()){
+//            foreach($data as $key=>$val){
+//                $model->$key=$val;
+//            }
+//            $model->save();
+//        }
+//    }
 
-    //分页获取数据
-    public static function getPaginate($where,$select,$sort,$desc,$paginate)
-    {
-        return self::where($where)
-            ->join('sort',function($join){
-                        $join->on('sort.sid','=','blog.sortid');
-                    })
-            ->select($select)
-            ->orderby($sort,$desc)
-            ->paginate($paginate);
-    }
+//    //分页获取数据
+//    public static function getPaginate($where,$select,$sort,$desc,$paginate)
+//    {
+//        return self::where($where)
+//            ->join('sort',function($join){
+//                        $join->on('sort.sid','=','blog.sortid');
+//                    })
+//            ->select($select)
+//            ->orderby($sort,$desc)
+//            ->paginate($paginate);
+//    }
 
 
 }

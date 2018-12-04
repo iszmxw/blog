b7afd41bf102513e0febbc6a1dc7d2e05f3cd105
mail@54zm.com <mail@54zm.com>
2018-12-04 18:16:24 +0800

    'index'

--- resources/views/admin/article_edit.blade.php
+++ resources/views/admin/article_edit.blade.php
@@ -45,6 +45,7 @@
                             <form method="post" class="form-horizontal" id="post_url" action="{{url('admin/ajax/article_edit_check')}}">
                                 <input type="hidden" id="_token" name="_token" value="{{csrf_token()}}">
                                 <input type="hidden" name="gid" value="{{$blog['gid']}}">
+                                <input type="hidden" id="filedata" name="filedata" value="{{ $attachment }}">
                                 <input type="hidden" id="article_image_upload" value="{{ url('admin/ajax/article_image_upload') }}">
                                 <div class="form-group">
                                     <label class="col-sm-1 control-label">标题</label>

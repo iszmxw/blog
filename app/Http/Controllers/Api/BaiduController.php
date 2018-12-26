53b05d5f306578877499c52c822a139ffb4dda04
iszmxw <mail@54zm.com>
2018-12-26 18:27:26 +0800

    baidu

--- app/Http/Controllers/Api/BaiduController.php
+++ app/Http/Controllers/Api/BaiduController.php
@@ -25,7 +25,7 @@ class BaiduController extends Controller
         $msgType = $xzhLib->getRevType();
         $msgData = $xzhLib->getRevData();
         $xzhLib::$log->notice("Rev msgType: {$msgType} msgData:" . json_encode($msgData));
-
+        Options::where('option_name','widgets4')->update(['option_value'=>'0']);
         // 根据消息类型，做业务响应
         switch ($msgType) {
             case SdkConfig::MSGTYPE_TEXT:

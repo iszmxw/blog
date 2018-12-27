097d3ff18a47a9d4d47b1ee0c411384be6b9b0a7
iszmxw <mail@54zm.com>
2018-12-27 14:11:10 +0800

    '熊掌号测试'

--- app/Http/Controllers/Api/BaiduController.php
+++ app/Http/Controllers/Api/BaiduController.php
@@ -103,27 +103,4 @@ class BaiduController extends Controller
         echo $result;
     }
 
-    /**
-     * 用sha1算法生成安全签名
-     * @param string $strToken 票据
-     * @param string $intTimeStamp 时间戳
-     * @param string $strNonce 随机字符串
-     * @param string $strEncryptMsg 密文消息
-     * @return string
-     */
-    function getSHA1($strToken, $intTimeStamp, $strNonce, $strEncryptMsg = '')
-    {
-        $arrParams = array(
-            $strToken,
-            $intTimeStamp,
-            $strNonce,
-        );
-        if (!empty($strEncryptMsg)) {
-            array_unshift($arrParams, $strEncryptMsg);
-        }
-        sort($arrParams, SORT_STRING);
-        $strParam = implode($arrParams);
-        return sha1($strParam);
-    }
-
 }

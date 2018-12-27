4f82f6ef96515b80f9da6263f4e932449ede954e
iszmxw <mail@54zm.com>
2018-12-27 13:17:17 +0800

    '熊掌号测试'

--- app/Http/Controllers/Api/BaiduController.php
+++ app/Http/Controllers/Api/BaiduController.php
@@ -34,51 +34,51 @@ class BaiduController extends Controller
                 // 文本类型 $content 开发者根据$msgData自己组织回复
                 $xzhLib->text(SdkConfig::REV_TEXT_DEFAULT_REPLY)->reply();
                 break;
-            case SdkConfig::MSGTYPE_EVENT:
-                // 事件类型（如不需跟开发者交互，返回success即可）
-                $event = $msgData['Event'];
-                switch ($event) {
-                    case SdkConfig::EVENT_SUBSCRIBE:
-                        $xzhLib->text(SdkConfig::EVENT_SUBSCRIBE)->reply();
-                        break;
-                    case SdkConfig::EVENT_UNSUBSCRIBE:
-                        $xzhLib->text(SdkConfig::EVENT_UNSUBSCRIBE)->reply();
-                        break;
-                    case SdkConfig::EVENT_MENU_VIEW:
-                        $xzhLib->text(SdkConfig::EVENT_MENU_VIEW)->reply();
-                        break;
-                    case SdkConfig::EVENT_MENU_CLICK:
-                        $xzhLib->text(SdkConfig::EVENT_MENU_CLICK)->reply();
-                        break;
-                    default:
-                        //TODO 如有新增事件，在补充
-                        $xzhLib->text(SdkConfig::REV_NOT_KNOW_MSGTYPE_REPLY)->reply();
-                }
-                break;
-            case SdkConfig::MSGTYPE_IMAGE:
-                // 返回一张图片，注意回复的图片 mediaId 需要先上传
-                $xzhLib->image(691654)->reply();
-                break;
-            case SdkConfig::MSGTYPE_VOICE:
-//                $xzhLib->voice(691654)->reply();
-                $xzhLib->text('waiting!!!')->reply();
-                break;
-            case SdkConfig::MSGTYPE_PAY:
-                $payEvent = $msgData['Event'];
-                switch ($payEvent) {
-                    case SdkConfig::EVENT_PAY_PAY:
-                        $xzhLib->pay(1)->reply();
-                        break;
-                    case SdkConfig::EVENT_PAY_REFUND:
-                        $xzhLib->refund()->reply();
-                        break;
-                    default:
-                        $xzhLib::$log->notice('pay event error; event:' . $payEvent);
-                        $xzhLib->text('')->reply();
-                }
-                break;
+//            case SdkConfig::MSGTYPE_EVENT:
+//                // 事件类型（如不需跟开发者交互，返回success即可）
+//                $event = $msgData['Event'];
+//                switch ($event) {
+//                    case SdkConfig::EVENT_SUBSCRIBE:
+//                        $xzhLib->text(SdkConfig::EVENT_SUBSCRIBE)->reply();
+//                        break;
+//                    case SdkConfig::EVENT_UNSUBSCRIBE:
+//                        $xzhLib->text(SdkConfig::EVENT_UNSUBSCRIBE)->reply();
+//                        break;
+//                    case SdkConfig::EVENT_MENU_VIEW:
+//                        $xzhLib->text(SdkConfig::EVENT_MENU_VIEW)->reply();
+//                        break;
+//                    case SdkConfig::EVENT_MENU_CLICK:
+//                        $xzhLib->text(SdkConfig::EVENT_MENU_CLICK)->reply();
+//                        break;
+//                    default:
+//                        //TODO如有新增事件，在补充
+//                        $xzhLib->text(SdkConfig::REV_NOT_KNOW_MSGTYPE_REPLY)->reply();
+//                }
+//                break;
+//            case SdkConfig::MSGTYPE_IMAGE:
+//                // 返回一张图片，注意回复的图片 mediaId 需要先上传
+//                $xzhLib->image(691654)->reply();
+//                break;
+//            case SdkConfig::MSGTYPE_VOICE:
+////                $xzhLib->voice(691654)->reply();
+//                $xzhLib->text('waiting!!!')->reply();
+//                break;
+//            case SdkConfig::MSGTYPE_PAY:
+//                $payEvent = $msgData['Event'];
+//                switch ($payEvent) {
+//                    case SdkConfig::EVENT_PAY_PAY:
+//                        $xzhLib->pay(1)->reply();
+//                        break;
+//                    case SdkConfig::EVENT_PAY_REFUND:
+//                        $xzhLib->refund()->reply();
+//                        break;
+//                    default:
+//                        $xzhLib::$log->notice('pay event error; event:' . $payEvent);
+//                        $xzhLib->text('')->reply();
+//                }
+//                break;
             default:
-                // TODO 如有新增消息类型，在补充
+                // TODO如有新增消息类型，在补充
                 $xzhLib->text(SdkConfig::REV_SUCCESS_REPLY)->reply();
         }
     }

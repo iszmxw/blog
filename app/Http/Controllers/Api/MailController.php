eb432d2bd927f42c52bd12a0ff52ab7bbe71a4c2
iszmxw <mail@54zm.com>
2018-12-29 11:04:57 +0800

    '百度资源提交'

--- app/Http/Controllers/Api/MailController.php
+++ app/Http/Controllers/Api/MailController.php
@@ -9,10 +9,20 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller{
     public function push_content(Request $request)
     {
-        dd($request);
+        $name = '傻妞';
+        $flag = Mail::send('test',['name'=>$name],function($message){
+            $to = '442246396@qq.com';
+            $message ->to($to)->subject('邮件测试');
+        });
+        if($flag){
+            echo '发送邮件成功，请查收！';
+        }else{
+            echo '发送邮件失败，请重试！';
+        }
     }
 }

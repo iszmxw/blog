6cbf24ae9812500d20116b11e16e51b5e6b897f5
iszmxw <mail@54zm.com>
2018-12-29 11:27:31 +0800

    '百度资源提交'

--- app/Http/Controllers/Api/MailController.php
+++ app/Http/Controllers/Api/MailController.php
@@ -8,12 +8,13 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\OrderShipped;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller{
     public function push_content(Request $request)
     {
-
+        Mail::to('442246396@qq.com')->send(new OrderShipped());
     }
 }

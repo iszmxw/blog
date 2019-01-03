ab338d80e46092e2fb62902da8f12f66da232f1e
iszmxw <mail@54zm.com>
2019-01-03 12:14:26 +0800

    '小程序'

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 |
 */
 Route::group(['namespace'=>'Api'],function(){
-    //微信订阅号(aszmxw)
+    //熊掌号推送(aszmxw)
     Route::group(['prefix'=>'baidu'],function(){
         //token基本配置
         Route::any('xiongzhang','BaiduController@xiongzhang');
@@ -27,30 +27,30 @@ Route::group(['namespace'=>'Api'],function(){
         Route::any('getAccessToken','WechatController@getAccessToken');
     });
 
-    //微信订阅号(aszmxw)
+    //发送邮件(aszmxw)
     Route::group(['prefix'=>'mail'],function(){
         //发送邮件
         Route::any('push_content','MailController@push_content');
     });
-});
+    //小程序
+    Route::prefix('wechat_xcx')->group(function () {
+        //消息推送接口设置
+        Route::get('message_push', 'WeChat\ApiController@message_push');
+        //获取access_token
+        Route::get('access_token', 'WeChat\ApiController@access_token');
+        Route::get('get_access_token', 'WeChat\ApiController@get_access_token');
+        //获取小程序码
+        Route::get('getwxacode', 'WeChat\ApiController@getwxacode');
+        //获取小程序二维码
+        Route::get('createwxaqrcode', 'WeChat\ApiController@createwxaqrcode');
+        //登录小程序
+        Route::get('login', 'WeChat\ApiController@login');
+        //测试专用
+        Route::get('test', 'WeChat\ApiController@test');
 
-Route::prefix('wechat_xcx')->group(function () {
-    //消息推送接口设置
-    Route::get('message_push', 'WeChat\ApiController@message_push');
-    //获取access_token
-    Route::get('access_token', 'WeChat\ApiController@access_token');
-    Route::get('get_access_token', 'WeChat\ApiController@get_access_token');
-    //获取小程序码
-    Route::get('getwxacode', 'WeChat\ApiController@getwxacode');
-    //获取小程序二维码
-    Route::get('createwxaqrcode', 'WeChat\ApiController@createwxaqrcode');
-    //登录小程序
-    Route::get('login', 'WeChat\ApiController@login');
-    //测试专用
-    Route::get('test', 'WeChat\ApiController@test');
-
-    //小程序首页
-    Route::get('index', 'WeChat\ApiController@index');
-    //小程序文章页面
-    Route::get('article', 'WeChat\ApiController@article');
+        //小程序首页
+        Route::get('index', 'WeChat\ApiController@index');
+        //小程序文章页面
+        Route::get('article', 'WeChat\ApiController@article');
+    });
 });

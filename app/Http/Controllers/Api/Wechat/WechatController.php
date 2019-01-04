fd6fef8289c26d53752d3c4906d22101128333db
iszmxw <mail@54zm.com>
2019-01-04 15:30:52 +0800

    '微信公众号'

--- app/Http/Controllers/Api/Wechat/WechatController.php
+++ app/Http/Controllers/Api/Wechat/WechatController.php
@@ -2,29 +2,26 @@
 
 namespace App\Http\Controllers\Api\Wechat;
 
-use App\Library\HttpCurl;
-use App\Models\Options;
-use App\Models\User;
-use App\Models\Userqq;
-use EasyWeChatComposer\EasyWeChat;
+use EasyWeChat\Factory;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
-use Illuminate\Support\Facades\Redis;
 
 class WechatController extends Controller
 {
     /**
-     * 处理微信的请求消息
-     *
-     * @return string
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @throws \EasyWeChat\Kernel\Exceptions\BadRequestException
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidArgumentException
+     * @throws \EasyWeChat\Kernel\Exceptions\InvalidConfigException
+     * @throws \ReflectionException
      */
     public function serve()
     {
-        $officialAccount = EasyWeChat::officialAccount(); // 公众号
+
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
-        $app = app('wechat.official_account');
+        $config = config('wechat.official_account');
+        $app = Factory::officialAccount($config);
         $app->server->push(function($message){
             return "欢迎关注 overtrue！";
         });

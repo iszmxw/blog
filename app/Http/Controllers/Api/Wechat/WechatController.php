2e88e6a1430f8bcfeba4bc8cfb3fbd5d0a8f137c
iszmxw <mail@54zm.com>
2019-01-04 22:50:58 +0800

    '微信公众号'

--- app/Http/Controllers/Api/Wechat/WechatController.php
+++ app/Http/Controllers/Api/Wechat/WechatController.php
@@ -21,7 +21,6 @@ class WechatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
         $config = config('wechat.official_account');
         $app = Factory::officialAccount($config);
-        $app->base->getValidIps();
         $app->server->push(function ($message) {
             return "您好！欢迎使用 EasyWeChat!您当前的微信服务器ip为：";
         });

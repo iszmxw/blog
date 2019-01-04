5fe7d05955c879e824257bc255b546c9341a8564
iszmxw <mail@54zm.com>
2019-01-04 22:33:15 +0800

    '微信公众号'

--- app/Http/Controllers/Api/Wechat/WechatController.php
+++ app/Http/Controllers/Api/Wechat/WechatController.php
@@ -21,7 +21,7 @@ class WechatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
         $config = config('wechat.official_account');
         $app = Factory::officialAccount($config);
-        $app->server->push(function($message){
+        $app->server->push(function(){
             return "欢迎关注追梦小窝的公众号，此公众号仅供提供一些服务！";
         });
 

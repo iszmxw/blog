c6e1422a49ed7d665c7ce8380ea276abfb11513f
iszmxw <mail@54zm.com>
2019-01-05 15:22:04 +0800

    '微信'

--- app/Http/Controllers/Api/Wechat/WechatController.php
+++ app/Http/Controllers/Api/Wechat/WechatController.php
@@ -18,6 +18,7 @@ class WechatController extends Controller
      */
     public function serve()
     {
+        dd(11);
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
         $config = config('wechat.official_account');
         $app = Factory::officialAccount($config);

ee33808756b8699de78c26e2e0f9393e405c30ed
iszmxw <mail@54zm.com>
2019-01-05 16:11:55 +0800

    '微信'

--- app/Http/Controllers/Api/Wechat/WechatController.php
+++ app/Http/Controllers/Api/Wechat/WechatController.php
@@ -10,14 +10,6 @@ use Illuminate\Support\Facades\Log;
 
 class WechatController extends Controller
 {
-    public $wechat;
-    public function __construct($app)
-    {
-        Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
-        $config = config('wechat.official_account');
-        $this->wechat = Factory::officialAccount($config);
-    }
-
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      * @throws \EasyWeChat\Kernel\Exceptions\BadRequestException
@@ -28,18 +20,18 @@ class WechatController extends Controller
     public function serve()
     {
         $config = config('wechat.official_account');
-        $this->wechat = Factory::officialAccount($config);
-        $this->wechat->server->push(function ($message) {
+        $app = Factory::officialAccount($config);
+        $app->server->push(function ($message) {
             return "您好！欢迎使用 公众号服务!";
         });
-        $response = $this->wechat->server->serve();
+        $response = $app->server->serve();
         // 将响应输出
         return $response;
     }
 
     public function oauth_callback(Request $request)
     {
-//        $list = $this->wechat->menu->list();
+//        $list = $app->menu->list();
 //        Options::EditData(['option_name'=>'test_info'],['option_value'=>serialize($list)]);
 //        Options::EditData(['option_name'=>'test_info'],['option_value'=>11]);
 //        dump($list);

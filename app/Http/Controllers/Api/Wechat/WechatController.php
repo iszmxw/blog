3680556dc83df6a18d81686d1215e0cd14207a0f
iszmxw <mail@54zm.com>
2019-01-05 16:35:38 +0800

    '微信'

--- app/Http/Controllers/Api/Wechat/WechatController.php
+++ app/Http/Controllers/Api/Wechat/WechatController.php
@@ -36,26 +36,21 @@ class WechatController extends Controller
         $buttons = [
             [
                 "type" => "click",
-                "name" => "今日歌曲",
-                "key"  => "V1001_TODAY_MUSIC"
+                "name" => "追梦小窝",
+                "url"  => "http://blog.54zm.com"
             ],
             [
-                "name"       => "菜单",
+                "name"       => "更多内容",
                 "sub_button" => [
                     [
                         "type" => "view",
-                        "name" => "搜索",
-                        "url"  => "http://www.soso.com/"
+                        "name" => "百度一下",
+                        "url"  => "http://www.baidu.com/"
                     ],
                     [
                         "type" => "view",
-                        "name" => "视频",
+                        "name" => "腾讯视频",
                         "url"  => "http://v.qq.com/"
-                    ],
-                    [
-                        "type" => "click",
-                        "name" => "赞一下我们",
-                        "key" => "V1001_GOOD"
                     ],
                 ],
             ],

8d915b0ccd98606b568f464580f5705d0eaeac5b
iszmxw <mail@54zm.com>
2019-01-05 17:10:03 +0800

    '微信'

--- app/Http/Controllers/Api/Wechat/WechatController.php
+++ app/Http/Controllers/Api/Wechat/WechatController.php
@@ -44,10 +44,11 @@ class WechatController extends Controller
 
             return $oauth->redirect();
             // 这里不一定是return，如果你的框架action不是返回内容的话你就得使用
+        }else{
+            // 已经登录过
+            $user = $_SESSION['wechat_user'];
+            return redirect('api/wechat/user/profile');
         }
-
-        // 已经登录过
-        $user = $_SESSION['wechat_user'];
 
     }
 

36031b0d995c3d6dc0018c0e68cfd329a2942ad9
iszmxw <mail@54zm.com>
2019-01-05 15:44:44 +0800

    '微信'

--- app/Http/Controllers/Api/Wechat/WechatController.php
+++ app/Http/Controllers/Api/Wechat/WechatController.php
@@ -22,12 +22,9 @@ class WechatController extends Controller
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
         $config = config('wechat.official_account');
         $app = Factory::officialAccount($config);
-//        $app->server->push(function ($message) {
-//            return "您好！欢迎使用 EasyWeChat!您当前的openid为：".$message['FromUserName'];
-//        });
-        $list = $app->menu->list();
-
-        Options::EditData(['option_name'=>'test_info'],['option_value'=>11]);
+        $app->server->push(function ($message) {
+            return "您好！欢迎使用 公众号服务!您当前的openid为：".$message['FromUserName'];
+        });
 
         $response = $app->server->serve();
         // 将响应输出
@@ -43,6 +40,7 @@ class WechatController extends Controller
 //            return "您好！欢迎使用 EasyWeChat!您当前的openid为：".$message['FromUserName'];
 //        });
         $list = $app->menu->list();
-        Options::EditData(['option_name'=>'test_info'],['option_value'=>serialize($list)]);
+//        Options::EditData(['option_name'=>'test_info'],['option_value'=>serialize($list)]);
+        Options::EditData(['option_name'=>'test_info'],['option_value'=>11]);
     }
 }

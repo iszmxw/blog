e145f87287d0ae5303314a2cb273db260128351b
iszmxw <mail@54zm.com>
2019-01-11 14:57:48 +0800

    'blog'

--- app/Library/IpAddress.php
+++ app/Library/IpAddress.php
@@ -3,13 +3,16 @@
 namespace App\Library;
 
 
+use GuzzleHttp\Client;
+
 class IpAddress
 {
 
     public static function address($ip)
     {
         $url = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query={$ip}&resource_id=6006";
-        $response = HttpCurl::doGet($url);
+        $client = new Client();
+        $response = $client->get($url)->getBody()->getContents();
         $response = mb_convert_encoding($response, 'utf-8','GB2312');
         $re = json_decode($response,true);
         if (empty($re['data'])){

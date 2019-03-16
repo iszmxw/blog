9fd1bb91b7b683ff42b3097f37f801a6825d975b
iszmxw <mail@54zm.com>
2019-03-16 15:22:22 +0800

    blog

--- app/Http/Controllers/Web/WallController.php
+++ app/Http/Controllers/Web/WallController.php
@@ -19,7 +19,7 @@ class WallController extends Controller
 {
     public function index()
     {
-        return view('wall.welcome');
+        return view('wall.index');
     }
 
     public function register()

e8794d227f22b3e0ae32c73463104e71d9d32be3
iszmxw <mail@54zm.com>
2019-03-20 17:57:30 +0800

    wx_mini

--- app/Http/Controllers/Api/Wechat/MiniController.php
+++ app/Http/Controllers/Api/Wechat/MiniController.php
@@ -56,10 +56,7 @@ class MiniController extends Controller
         $url = 'https://api.weixin.qq.com/wxa/getwxacode?access_token='.$access_token;
         $client = new Client();
         $re = $client->post($url, [
-            'path' => 128,
-            'width' => 450,
-            'auto_color' => true,
-            'is_hyaline' => false,
+            'path' => 128
         ])->getBody();
         return $re;
     }

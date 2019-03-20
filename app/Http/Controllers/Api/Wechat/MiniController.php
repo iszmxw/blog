40758afa2228a0191463fecff851bb336ca5850e
iszmxw <mail@54zm.com>
2019-03-20 17:48:34 +0800

    wx_mini

--- app/Http/Controllers/Api/Wechat/MiniController.php
+++ app/Http/Controllers/Api/Wechat/MiniController.php
@@ -60,7 +60,8 @@ class MiniController extends Controller
             'auto_color' => true,
             'is_hyaline' => false,
         ];
-//        $data = json_encode($data, JSON_UNESCAPED_UNICODE);
+        $data = json_encode($data, JSON_UNESCAPED_UNICODE);
+        dd($data);
         $client = new Client();
         $re = $client->post($url, $data)->getBody();
         return $re;

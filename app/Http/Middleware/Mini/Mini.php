98644191396c6141e60d1e9628fe1d327db24e1c
iszmxw <mail@54zm.com>
2019-03-20 11:00:57 +0800

    wx_mini

--- app/Http/Middleware/Mini/Mini.php
+++ app/Http/Middleware/Mini/Mini.php
@@ -10,34 +10,50 @@ class Mini
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
         $data = $request->all();
         $route = $request->path();
-        switch ($route){
+        switch ($route) {
             case 'api/wx_mini/login':
                 break;
             default :
-                return self::CheckToken($data,$request);
+                $re = self::CheckToken($data);
+
+                return self::format_response($re, $next);
+
         }
+
         return $next($request);
     }
 
-    public static function CheckToken($data,$request)
+    public static function CheckToken($data)
     {
-        if(empty($data['token'])){
-            return response()->json(['status'=>'0','msg'=>'token不能为空','data'=>[]]);
-        }else{
+        if (empty($data['token'])) {
+            return response()->json(['status' => '0', 'msg' => 'token不能为空', 'data' => []]);
+        } else {
             $redis = Redis::connection('blog_web')->get($data['token']);
-            if (empty($redis)){
-                return response()->json(['status'=>'-100','msg'=>'登录状态失效，请重新登录','data'=>[]]);
-            }else{
-                return $request;
+            if (empty($redis)) {
+                return response()->json(['status' => '-100', 'msg' => '登录状态失效，请重新登录', 'data' => []]);
+            } else {
+                $redis_nfo = decrypt($redis);
+                dump($redis_nfo);
             }
+        }
+    }
+
+
+    //switch中使用的返回方法
+    public static function format_response($data, Closure $next)
+    {
+        if ($data['status'] == '0') {
+            return response()->json($data);
+        } else{
+            return $next($data);
         }
     }
 }

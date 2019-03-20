cdb54c513b791af95dbbed5d02f8c643cafe4950
iszmxw <mail@54zm.com>
2019-03-20 09:53:53 +0800

    wx_mini

--- app/Http/Middleware/Mini/Mini.php
+++ app/Http/Middleware/Mini/Mini.php
@@ -16,7 +16,7 @@ class Mini
     public function handle($request, Closure $next)
     {
         $token = $request->all();
-        $route = $request->route()->getName();
+        $route = $request->getRequestUri();
         dd($route,$token,$request);
         return $next($request);
     }

517046a87a07e3cecbe8c6ad87ae00efeb526042
追梦小窝 <mail@54zm.com>
2019-07-15 20:05:56 +0800

    master

---
+++ public/style/admin/inspinia/js/plugins/toastr/toastr_style.js
@@ -0,0 +1,17 @@
+//给定样式规则
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "progressBar": true,
+    "preventDuplicates": false,
+    "positionClass": "toast-top-center",
+    "onclick": null,
+    "showDuration": "400",
+    "hideDuration": "1000",
+    "timeOut": "3000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+};

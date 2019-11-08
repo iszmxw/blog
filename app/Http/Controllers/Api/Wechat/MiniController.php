faa0180fdf7aa05398b355f2a6bc97548769f5f3
iszmxw <mail@54zm.com>
2019-11-08 14:24:27 +0800

    先下拉=>后上传

--- app/Http/Controllers/Api/Wechat/MiniController.php
+++ app/Http/Controllers/Api/Wechat/MiniController.php
@@ -343,6 +343,7 @@ class MiniController extends Controller
         $category_id ? $where[] = ['sort_id', $category_id] : '';
         $list = Blog::where($where)->select('id', 'title', 'created_at')->limit($pagesize)->orderby('created_at', 'DESC')->get();
         foreach ($list as $key => $val) {
+            dd($val->created_at);
             $val->created_at = date('Y-m-d', $val->created_at);
         }
         if ($pagesize - $list->count() > 10) {

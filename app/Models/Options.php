db24c44422060763d660d16c605fdd6afa23f682
iszmxw <mail@54zm.com>
2019-11-16 09:53:30 +0800

    先下拉=>后上传

--- app/Models/Options.php
+++ app/Models/Options.php
@@ -9,7 +9,7 @@ class Options extends Defaults
     protected $table = 'options';
     protected $primaryKey = 'option_id';
     //设置时间戳字段
-    public $timestamps = false;
+    public $timestamps= false;
     // 以时间戳的形式来维护
     public $dateFormat = false;
     //过滤黑名单字段

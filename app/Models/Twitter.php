f6732fd4f938e9a73787d5320f7e30200d930bfd
iszmxw <mail@54zm.com>
2019-11-16 09:57:28 +0800

    先下拉=>后上传

--- app/Models/Twitter.php
+++ app/Models/Twitter.php
@@ -2,8 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 class Twitter extends Defaults
 {
+    use SoftDeletes;
+
+    //设置时间戳字段
+    public $timestamps = true;
+    // 以时间戳的形式来维护
+    public $dateFormat = 'U';
+    //过滤黑名单字段
+    public $guarded = [];
+
     protected $table = 'twitter';
     protected $primaryKey = 'id';
 

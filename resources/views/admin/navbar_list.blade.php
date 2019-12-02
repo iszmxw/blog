4a3e519daeab6f5e2c272b25d6d196fe86be3f03
iszmxw <mail@54zm.com>
2019-12-02 11:43:53 +0800

    优化后台

--- resources/views/admin/navbar_list.blade.php
+++ resources/views/admin/navbar_list.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ config('app.locale') }}">
 <head>
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>追梦小窝 | 后台首页</title>
     @include('admin.public.common_css')

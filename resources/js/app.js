bef79fbc8eac0eb60916e10b961a398a9ee10872
iszmxw <mail@54zm.com>
2019-12-02 10:08:00 +0800

    优化后台

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,15 @@
  */
 
 require('./bootstrap');
+
+window.Vue = require('vue');
+
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});

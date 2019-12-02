7e7efb06683341749e6032160124456959aa3f7e
iszmxw <mail@54zm.com>
2019-12-02 12:57:05 +0800

    优化后台

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ app()->getLocale() }}">
+<html lang="{{ config('app.locale') }}">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">

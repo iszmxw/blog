be4cfbf8cb3d77efa53a9c0901bf2ce058850414
iszmxw <mail@54zm.com>
2019-12-02 12:47:28 +0800

    优化后台

--- resources/views/admin/category_list.blade.php
+++ resources/views/admin/category_list.blade.php
@@ -314,31 +314,3 @@
         }
     </script>
 @endsection
-
-
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>追梦小窝 | </title>
-    @include('admin.public.common_css')
-</head>
-<body class="">
-<div id="wrapper">
-    {{--侧边栏--}}
-    @include('admin.public.nav')
-    <div id="page-wrapper" class="gray-bg">
-        {{--头部--}}
-        @include('admin.public.header')
-        <div class="wrapper wrapper-content">
-
-
-        </div>
-        {{--底部--}}
-        @include('admin.public.footer')
-    </div>
-</div>
-
-</body>
-</html>

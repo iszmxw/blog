5b28bd92906e1b04d15963d2c8a66bd4a1c90eb8
iszmxw <mail@54zm.com>
2019-12-03 09:49:15 +0800

    优化后台

--- resources/views/admin/public/common_css.blade.php
+++ resources/views/admin/public/common_css.blade.php
@@ -4,3 +4,5 @@
 <link href="{{asset('style/admin/inspinia/css/animate.css')}}" rel="stylesheet">
 <link href="{{asset('style/admin/inspinia/css/style.css')}}" rel="stylesheet">
 <link href="{{asset('style/admin/inspinia/css/plugins/sweetalert/sweetalert.css')}}" rel="stylesheet">
+<!-- 引入样式 -->
+<link rel="stylesheet" href="https://unpkg.com/element-ui/lib/theme-chalk/index.css">

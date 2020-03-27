64e1b934cdaec8fde01f2d2dd8636fc16c1877b7
追梦小窝 <mail@54zm.com>
2020-03-27 09:58:05 +0800

    优化后台

--- app/Http/Middleware/Admin/ApiAdmin.php
+++ app/Http/Middleware/Admin/ApiAdmin.php
@@ -3,8 +3,6 @@
 namespace App\Http\Middleware\Admin;
 
 use App\Library\Tools;
-use App\Models\Role;
-use App\Models\RoleRoute;
 use Closure;
 use Illuminate\Support\Facades\Cache;
 
@@ -34,7 +32,6 @@ class ApiAdmin
                 break;
         }
     }
-
 
     // 登录状态监测
     public static function LoginCheck($request)
@@ -76,60 +73,6 @@ class ApiAdmin
             return self::ReArray(1, $request);
         } else {
             return self::ReArray(0, ['code' => 508, 'message' => '登录过期，无法获取用户详细信息。请您退出后重新登录']);
-        }
-    }
-
-    // 检测用户的登录和角色权限
-    public static function LoginAndRoleCheck($request)
-    {
-        $res = self::LoginCheck($request);
-        if ($res['status'] == 1) {
-            // 如果登录成功检测用户的权限
-            return self::RoleCheck($request);
-        } else {
-            return $res;
-        }
-    }
-
-
-    // 用户角色权限检测
-    public static function RoleCheck($request)
-    {
-        $route = $request->path();
-        // 从头部获取token
-        $Xtoken = $request->header('Admin-Token');
-        // 接收第一次传过来的token
-        $token = $request->get('token');
-        // token最终结果
-        $token = empty($token) ? $Xtoken : $token;
-        $info  = Cache::get($token);
-        // 查找用户可访问路由
-        if (empty($info['allow_routes'])) {
-            $allow_routes = [];
-            $role_id      = $info['role_id'];
-            $routes       = Role::getValue(['id' => $role_id], 'routes');
-            $routes       = explode(',', $routes);
-            $routes       = RoleRoute::where('route', '<>', null)->whereIn('id', $routes)->get(['route'])->toArray();
-            foreach ($routes as $key => $val) {
-                $allow_routes[] = $val['route'];
-            }
-            $public_route = config('route.admin');
-            // 计算出用户所有允许访问的路由
-            $allow_routes = array_merge($allow_routes, $public_route);
-            // 缓存角色可访问路由
-            $info['allow_routes'] = $allow_routes;
-            // 更新登录信息，同时缓存从新开始，1小时后失效
-            Cache::put($token, $info, 3600);
-        } else {
-            $allow_routes = $info['allow_routes'];
-        }
-        // 判断角色是否具备当前请求的路由
-        if ($info['id'] != 1 && !in_array($route, $allow_routes)) {
-            // 默认所有用户具备权限
-//            return self::ReArray(1, $request);
-            return self::ReArray(0, ['code' => 500, 'message' => '抱歉！您不具备权限！']);
-        } else {
-            return self::ReArray(1, $request);
         }
     }
 

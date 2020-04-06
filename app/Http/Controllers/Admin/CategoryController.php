ca7d3c6073e7ef212941970a6f5a7a0d2c2284fd
追梦小窝 <mail@54zm.com>
2020-04-06 23:23:37 +0800

    优化后台

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\DB;
+use Ramsey\Uuid\Uuid;
+
+class CategoryController extends Controller
+{
+    // 栏目列表
+    public function category_list(Request $request)
+    {
+
+    }
+
+
+    // 添加栏目
+    public function category_add(Request $request)
+    {
+
+    }
+
+    // 编辑栏目
+    public function category_edit(Request $request)
+    {
+
+    }
+
+    // 删除栏目
+    public function category_delete(Request $request)
+    {
+
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -23,6 +23,40 @@ Route::group(['namespace' => 'Admin'], function () {
         // 系统首页
         Route::any('dashboard', 'DashboardController@dashboard');
 
+
+        // 系统管理
+        Route::group(['prefix' => 'system'], function () {
+            // 获取网站系统配置
+            Route::any('config', 'SystemController@config');
+            // 保存网站配置
+            Route::any('save_config', 'SystemController@save_config');
+            // 访客记录
+            Route::any('view_log', 'SystemController@view_log');
+        });
+
+
+        // 栏目管理
+        Route::group(['prefix' => 'category'], function () {
+            // 栏目列表
+            Route::any('category_list', 'CategoryController@category_list');
+            // 添加栏目
+            Route::any('category_add', 'CategoryController@category_add');
+            // 编辑栏目
+            Route::any('category_edit', 'CategoryController@category_edit');
+            // 删除栏目
+            Route::any('category_delete', 'CategoryController@category_delete');
+        });
+
+
+        // 首页导航管理
+        Route::group(['prefix' => 'navbar'], function () {
+            // 导航列表
+            Route::any('navbar_list', 'CategoryController@navbar_list');
+            // 添加导航
+            Route::any('navbar_add', 'CategoryController@navbar_add');
+        });
+
+
     });
 });
 

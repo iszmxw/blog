0635de6efea68ac989fce7d9619f6427cd1f9a33
追梦小窝 <mail@54zm.com>
2020-04-07 19:54:27 +0800

    优化后台

--- app/Http/Controllers/Admin/SystemController.php
+++ app/Http/Controllers/Admin/SystemController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Options;
 use App\Models\User;
+use App\Models\ViewLog;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Cache;
@@ -14,17 +16,18 @@ class SystemController extends Controller
     // 获取网站系统配置
     public function config(Request $request)
     {
-        $info = $request->get('info');
-        // 文章数量
-
-        // 评论数量
-
-        // 微语数量
-
-        // 系统信息
-
-        // 登录日志记录
-        $data = [];
+        $site_title       = Options::getOption('site_title');
+        $site_description = Options::getOption('site_description');
+        $site_url         = Options::getOption('site_url');
+        $icp              = Options::getOption('icp');
+        $footer_info      = Options::getOption('footer_info');
+        $data             = [
+            'site_title'       => $site_title,
+            'site_description' => $site_description,
+            'site_url'         => $site_url,
+            'icp'              => $icp,
+            'footer_info'      => $footer_info,
+        ];
         return ['code' => 200, 'message' => 'ok', 'data' => $data];
     }
 
@@ -32,22 +35,38 @@ class SystemController extends Controller
     /**
      * 保存网站配置
      * @param Request $request
+     * @return array
+     * @throws \Exception
      * @author: iszmxw <mail@54zm.com>
-     * @Date：2020/4/6 23:14
+     * @Date：2020/4/7 19:46
      */
     public function save_config(Request $request)
     {
-
+        $param = $request->all();
+        DB::beginTransaction();
+        try {
+            foreach ($param as $key => $val) {
+                Options::SaveOption($key, $val);
+            }
+            DB::commit();
+            return ['code' => 200, 'message' => '操作成功！'];
+        } catch (\Exception $e) {
+            dd($e);
+            DB::rollback();
+            return ['code' => 500, 'message' => '操作失败请稍后再试！'];
+        }
     }
 
     /**
      * 访客记录
      * @param Request $request
+     * @return array
      * @author: iszmxw <mail@54zm.com>
-     * @Date：2020/4/6 23:15
+     * @Date：2020/4/7 19:52
      */
     public function view_log(Request $request)
     {
-
+        $view_log = ViewLog::getPaginate([], [], 10, 'id', 'DESC');
+        return ['code' => 200, 'message' => 'ok', 'data' => $view_log];
     }
 }

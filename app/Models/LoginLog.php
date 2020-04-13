a844a92928cbba2a273bcb28eaa30345d3c7aa6c
追梦小窝 <mail@54zm.com>
2020-04-13 20:59:59 +0800

    优化后台

--- app/Models/LoginLog.php
+++ app/Models/LoginLog.php
@@ -9,14 +9,26 @@ class LoginLog extends Defaults
 {
     use SoftDeletes;
 
-    //设置时间戳字段
+    // 设置时间戳字段
     public $timestamps = true;
     // 以时间戳的形式来维护
     public $dateFormat = 'U';
-    //过滤黑名单字段
+    // 过滤黑名单字段
     public $guarded = [];
 
     protected $table = 'login_log';
     protected $primaryKey = 'id';
+
+    /**
+     * 格式化返回时间戳
+     * @param $time
+     * @return false|string
+     * @author: iszmxw <mail@54zm.com>
+     * @Date：2020/4/13 20:56
+     */
+    public function getCreatedAtAttribute($time)
+    {
+        return date('Y-m-d H:i:s', $time);
+    }
 
 }

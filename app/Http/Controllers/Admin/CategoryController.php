1a1d8736f7b2cc849b3acebf760592c14af867a5
追梦小窝 <mail@54zm.com>
2020-04-20 13:42:30 +0800

    优化后台

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -26,25 +26,69 @@ class CategoryController extends Controller
     }
 
 
-    // 添加栏目
+    /**
+     * 添加栏目
+     * @param Request $request
+     * @return array
+     * @throws \Exception
+     * @author: iszmxw <mail@54zm.com>
+     * @Date：2020/4/20 12:07
+     */
     public function category_add(Request $request)
     {
-
+        $data = $request->all();
+        DB::beginTransaction();
+        try {
+            Sort::AddData($data);
+            DB::commit();
+            return ['code' => 200, 'message' => '添加栏目成功！'];
+        } catch (\Exception $e) {
+            DB::rollback();
+            return ['code' => 500, 'message' => '添加栏目失败，请稍后再试！'];
+        }
     }
 
-    // 编辑栏目
+    /**
+     * 编辑栏目
+     * @param Request $request
+     * @return array
+     * @throws \Exception
+     * @author: iszmxw <mail@54zm.com>
+     * @Date：2020/4/20 12:04
+     */
     public function category_edit(Request $request)
     {
-        $id          = $request->get('id');
-        $name        = $request->get('name');
-        $pid         = $request->get('pid');
-        $description = $request->get('description');
-        $alias       = $request->get('alias');
+        $data = $request->all();
+        DB::beginTransaction();
+        try {
+            Sort::EditData(['id' => $data['id']], $data);
+            DB::commit();
+            return ['code' => 200, 'message' => '编辑栏目成功！'];
+        } catch (\Exception $e) {
+            DB::rollback();
+            return ['code' => 500, 'message' => '编辑栏目失败，请稍后再试！'];
+        }
     }
 
-    // 删除栏目
+    /**
+     * 删除栏目
+     * @param Request $request
+     * @return array
+     * @throws \Exception
+     * @author: iszmxw <mail@54zm.com>
+     * @Date：2020/4/20 12:05
+     */
     public function category_delete(Request $request)
     {
-
+        $data = $request->all();
+        DB::beginTransaction();
+        try {
+            Sort::selected_delete($data);
+            DB::commit();
+            return ['code' => 200, 'message' => '删除栏目成功！'];
+        } catch (\Exception $e) {
+            DB::rollback();
+            return ['code' => 500, 'message' => '删除栏目失败，请稍后再试！'];
+        }
     }
 }

34efb44c54d105e52b20b3583672fe5f8a7606fb
追梦小窝 <mail@54zm.com>
2020-05-07 17:29:59 +0800

    优化后台

---
+++ resources/views/wall/qq.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>追梦小窝 | QQ登记处</title>
+    <link href="{{asset('style/admin/inspinia/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('style/admin/inspinia/font-awesome/css/font-awesome.css')}}" rel="stylesheet">
+    <link href="{{asset('style/admin/inspinia/css/animate.css')}}" rel="stylesheet">
+    <link href="{{asset('style/admin/inspinia/css/style.css')}}" rel="stylesheet">
+</head>
+
+<body class="gray-bg">
+<div class="passwordBox animated fadeInDown">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="ibox-content">
+
+                <h2 class="font-bold">QQ登记</h2>
+
+                <p>
+                    输入您的登陆的QQ号码，您的号码将被登记在此，以便日后联系。
+                </p>
+
+                <div class="row">
+
+                    <div class="col-lg-12">
+                        <form class="m-t" role="form" action="">
+                            <div class="form-group">
+                                <input type="number" class="form-control" placeholder="请输入您登陆的QQ号码，方便以后联系" required="">
+                            </div>
+                            <button type="submit" class="btn btn-primary block full-width m-b">确认此QQ</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <hr/>
+    <div class="row">
+        <div class="col-md-6">
+            <small>Copyright 追梦小窝 &copy; 2019-2021</small>
+        </div>
+        <div class="col-md-6 text-right">
+            blog.54zm.com
+        </div>
+    </div>
+</div>
+
+</body>
+
+</html>

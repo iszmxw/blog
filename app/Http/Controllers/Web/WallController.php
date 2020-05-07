53ec30cdd4d81da40f2c407feced102d6982e07f
追梦小窝 <mail@54zm.com>
2020-05-07 17:48:06 +0800

    优化后台

--- app/Http/Controllers/Web/WallController.php
+++ app/Http/Controllers/Web/WallController.php
@@ -40,7 +40,7 @@ class WallController extends Controller
             $hash1     = $hasher->hash($img1);
             $hash2     = $hasher->hash($img2);
             $distance1 = $hasher->distance($hash1, $hash2);
-            if ($distance1 < 15) {
+            if ($distance1 == 0) {
                 $res = Userqq::EditData(['openid' => $qq_data['openid']], ['qq' => $qq]);
                 session(['qq_data' => $res]);
                 return view('wall.index');

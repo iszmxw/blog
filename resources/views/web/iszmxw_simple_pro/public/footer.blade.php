f89833e698cb504a8ef7e2333b71a658066d68bc
追梦小窝 <mail@54zm.com>
2020-06-16 16:23:22 +0800

    优化后台

--- resources/views/web/iszmxw_simple_pro/public/footer.blade.php
+++ resources/views/web/iszmxw_simple_pro/public/footer.blade.php
@@ -1,6 +1,6 @@
 <div class="p-footer-box">
     <div class="p-footer-box-left">
-        © 2020 追梦小窝版权所有 豫ICP备18035337号
+        <strong>Copyright</strong> 追梦小窝 © 2017-2020 <a href="http://www.beian.miit.gov.cn/">鄂ICP备14019660号-1</a>
     </div>
     <div class="p-footer-box-right"></div>
 </div>

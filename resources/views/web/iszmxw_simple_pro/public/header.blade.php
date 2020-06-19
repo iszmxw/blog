7efeb772f261d1c9ccdd2408c94d2c77df085d87
追梦小窝 <mail@54zm.com>
2020-06-19 19:27:15 +0800

    优化后台

--- resources/views/web/iszmxw_simple_pro/public/header.blade.php
+++ resources/views/web/iszmxw_simple_pro/public/header.blade.php
@@ -4,12 +4,12 @@
         <a id="phoneOnOffMenu" style="display: none"><i class="layui-icon"></i> </a>
     </div>
     <div class="p-header-box-search-box">
-        <form action="/article/search" data-pjax-state="">
+        <form action="{{url('/article/search')}}">
             <input type="text" placeholder="请输入搜索内容" name="keyword" required="">
             <button type="submit"><i class="layui-icon"></i></button>
         </form>
     </div>
-    <a class="phone-title pjax" href="/" data-pjax-state="">
+    <a class="phone-title pjax" href="/">
         <i class="fa fa-snowflake-o"></i>
         <span class="p-left-logo-box-text-name">{{$user_data['nickname']}}</span>
     </a>
@@ -18,21 +18,21 @@
             <div id="skPlayer"></div>
         </div>
         <div class="p-header-box-login-box">
-            <a class="p-header-box-login-box-login" href="/user/login">登录</a>
+            <a class="p-header-box-login-box-login" href="{{url('wall/register')}}">登录</a>
             <span style="float: left;line-height: 50px;">|</span>
-            <a class="p-header-box-login-box-register" href="/user/register">注册</a>
+            <a class="p-header-box-login-box-register" href="{{url('wall/register')}}">注册</a>
         </div>
         <a id="phoneSetting" style="display: none"> <i class="layui-icon"></i></a>
     </div>
     <div class="p-phone-top-nav box-shadow">
-        <form action="/article/search" data-pjax-state="">
+        <form action="{{url('/article/search')}}">
             <input type="text" placeholder="请输入搜索内容" name="keyword" required="">
             <button type="submit"><i class="layui-icon"></i></button>
         </form>
         <div class="p-phone-top-nav-login-box">
             <div class="no-login layui-clear">
-                <a class="login-btn" href="/user/login">登录</a>
-                <a class="register-btn" href="/user/register">注册</a>
+                <a class="login-btn" href="{{url('wall/register')}}">登录</a>
+                <a class="register-btn" href="{{url('wall/register')}}">注册</a>
             </div>
         </div>
     </div>

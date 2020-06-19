1fb9e83689bc5d8e54b7e4fde4759240f4e23347
追梦小窝 <mail@54zm.com>
2020-06-19 20:51:45 +0800

    优化后台

--- resources/views/web/iszmxw_simple_pro/public/style.blade.php
+++ resources/views/web/iszmxw_simple_pro/public/style.blade.php
@@ -1,4 +1,4 @@
-<link rel="icon" type="image/ico" href="{{asset('style/web/iszmxw_simple_pro/images/ec83d6ed69d84517beb3ee9ccc49f8b4.jpg')}}">
+<link rel="icon" type="image/ico" href="{{asset('style/web/iszmxw_simple_pro/static/images/qzone.png')}}">
 <link rel="stylesheet" href="{{asset('style/web/iszmxw_simple_pro/static/plugins/layui/css/layui.css')}}" media="all">
 <link rel="stylesheet" href="{{asset('style/web/iszmxw_simple_pro/static/css/perfree.min.css')}}" media="all">
 <link rel="stylesheet" href="{{asset('style/web/iszmxw_simple_pro/static/plugins/font-awesome/css/font-awesome.min.css')}}" media="all">

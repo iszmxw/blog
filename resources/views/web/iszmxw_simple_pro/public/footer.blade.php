640caa41a300cf7c245785df368b8974427368c8
追梦小窝 <mail@54zm.com>
2020-06-21 19:27:03 +0800

    优化后台

--- resources/views/web/iszmxw_simple_pro/public/footer.blade.php
+++ resources/views/web/iszmxw_simple_pro/public/footer.blade.php
@@ -3,6 +3,7 @@
         <strong>Copyright</strong> 追梦小窝 © 2017-2020 <a href="http://www.beian.miit.gov.cn/">鄂ICP备14019660号-1</a>
     </div>
     <div class="p-footer-box-right">
+        <span>站长统计</span>
         <script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? "https://" : "http://");
             document.write(unescape("%3Cspan id='cnzz_stat_icon_1276750286'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s96.cnzz.com/z_stat.php%3Fid%3D1276750286%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
     </div>

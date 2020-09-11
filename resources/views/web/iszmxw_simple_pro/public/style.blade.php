4baf920c263ad3e587c4dc08943a394426e32b8a
yezhiming <mail@54zm.com>
2020-09-11 11:25:59 +0800

    优化后台

--- resources/views/web/iszmxw_simple_pro/public/style.blade.php
+++ resources/views/web/iszmxw_simple_pro/public/style.blade.php
@@ -5,3 +5,4 @@
 <link rel="stylesheet" href="{{asset('style/web/iszmxw_simple_pro/static/plugins/skplayer/skPlayer.min.css')}}" media="all">
 <link rel="dns-prefetch" href="//cdn.bootcss.com">
 <link rel="stylesheet" href="{{asset('style/web/iszmxw_simple_pro/static/plugins/highlight/styles/vs.css')}}" media="all">
+<link href='http://cdn.repository.webfont.com/webfonts/nomal/140819/19673/5f5aedeef629d82988a7c766.css' rel='stylesheet' type='text/css' />

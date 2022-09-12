fd460b1366bfb360b481f308fd63a3a9b653ac56
John Yep <mail@54zm.com>
2022-09-12 18:39:51 +0800

    优化后台

--- resources/views/web/iszmxw_simple_pro/public/style.blade.php
+++ resources/views/web/iszmxw_simple_pro/public/style.blade.php
@@ -1,7 +1,6 @@
 <link rel="icon" type="image/ico" href="{{asset('style/web/iszmxw_simple_pro/static/images/qzone.png')}}">
 <link rel="stylesheet" href="{{asset('style/web/iszmxw_simple_pro/static/plugins/layui/css/layui.css')}}">
-<link rel="stylesheet"
-      href="{{asset('style/web/iszmxw_simple_pro/static/plugins/font-awesome/css/font-awesome.min.css')}}">
+<link rel="stylesheet" href="{{asset('style/web/iszmxw_simple_pro/static/plugins/font-awesome/css/font-awesome.min.css')}}">
 {{--一款简约好看的HTML5音乐播放器--}}
 <link rel="stylesheet" href="{{asset('style/web/iszmxw_simple_pro/static/plugins/skplayer/skPlayer.min.css')}}">
 {{--编辑器样式--}}
